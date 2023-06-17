0850dc205dab0aeef72c52adf9fd2a2315262636
Turbobot <turbobot@vercel.com>
2023-06-17 21:29:19 +0200

    feat(create-turbo): create basic

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import * as React from "react";
+
+export const Button = () => {
+  return <button onClick={() => alert("boop")}>Boop</button>;
+};
---
+++ packages/ui/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+// component exports
+export * from "./Button";
+export * from "./Header";
