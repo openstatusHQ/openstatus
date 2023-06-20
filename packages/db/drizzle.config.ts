dac56a285ff3db148203c4c05109fefec2342394
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-06-20 13:57:15 +0200

    drizzle init

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,4 +3,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/schema/index.ts",
   out: "./drizzle",
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL || "",
+  }
 } satisfies Config;
--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -4,26 +4,12 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 import * as schema from "./schema";
 
-export const createDatabase = (c: {
-  host: string;
-  username: string;
-  password: string;
-}) =>
-  drizzle(
-    connect({
-      ...c,
-      fetch: (url: string, init: any) => {
-        (init as any)["cache"] = undefined; // Remove cache header
-        return fetch(url, init);
-      },
-    }),
-    {
-      schema,
-    }
-  );
+const config = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+};
 
-export const db = createDatabase({
-  host: process.env.DATABASE_HOST!,
-  username: process.env.DATABASE_USERNAME!,
-  password: process.env.DATABASE_PASSWORD!,
-});
+const connection = connect(config);
+
+export const db = drizzle(connection);
