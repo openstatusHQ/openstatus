dc579da7c8c8608c32d99cb67559c8382c16a75d
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-06-20 20:20:26 +0200

    ✏️

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,13 @@
-"use client";
-
 import { Badge } from "@/components/ui/badge";
-import { Input } from "@/components/ui/input";
-import { addToWaitlist } from "./action";
-import { SubmitButton } from "./_components/submit-button";
-import { toast } from "@/components/ui/use-toast";
+
+import { Metadata } from "next";
+import { HeroForm } from "./_components/hero-form";
+
+export const metadata: Metadata = {
+  title: "Open-source monitoring service",
+  description:
+    "OpenStatus is an open source alternative to your current monitoring service with beautiful status page",
+};
 
 export default function Page() {
   return (
@@ -12,38 +15,15 @@ export default function Page() {
       <div className="flex-1 flex flex-col justify-center">
         <div className="mx-auto max-w-xl text-center">
           <div className="rounded-lg border border-border backdrop-blur-[2px] p-8">
-            <Badge>Announcing Post</Badge>
+            <Badge>Coming Soon</Badge>
             <h1 className="text-3xl text-foreground font-cal mb-6 mt-2">
-              OpenStatus
+              Open-source monitoring service
             </h1>
             <p className="text-muted-foreground mb-4">
-              Your Open Source Status Page.
+              OpenStatus is an open source alternative to your current
+              monitoring service with beautiful status page.
             </p>
-            <form
-              action={async (data) => {
-                try {
-                  const number = await addToWaitlist(data);
-                  const formattedNumber = Intl.NumberFormat().format(number);
-                  toast({
-                    description: `Thank you, you're number ${formattedNumber} on the list.`,
-                  });
-                } catch (e) {
-                  toast({
-                    description: "Something went wrong",
-                  });
-                }
-              }}
-              className="flex gap-1.5"
-            >
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="me@domain.com"
-                required
-              />
-              <SubmitButton />
-            </form>
+            <HeroForm />
           </div>
         </div>
       </div>
