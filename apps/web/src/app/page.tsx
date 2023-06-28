1190ee94b0377ca73171b61bbdc98185c4081b37
Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>
2023-06-28 22:28:13 +0200

    feat: input search (#8)
    * chore: extend tiny schema and improve cron
    
    * chore: small adjustments
    
    * feat: input search
    
    * wip: small changes
    
    * fix: state issue on mobile and url
    
    * wip: cache-control and escape key down
    
    * chore: small adjustments

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,8 +2,8 @@ import { Badge } from "@/components/ui/badge";
 import { HeroForm } from "./_components/hero-form";
 import { Tinybird, getResponseList } from "@openstatus/tinybird";
 import { env } from "@/env.mjs";
-import { StatusContainer } from "./_components/status-container";
 import MOCK from "@/app/_mocks/response-list.json";
+import { TableInputContainer } from "./_components/table-input-container";
 
 const tb = new Tinybird({ token: env.TINY_BIRD_API_KEY });
 
@@ -30,8 +30,10 @@ export default async function Page() {
             <HeroForm />
           </div>
         </div>
-        <div className="md:fixed bottom-8 right-8 max-w-max z-10">
-          <StatusContainer events={data} />
+        <div className="mx-auto max-w-xl w-full z-10 backdrop-blur-[2px]">
+          <div className="p-8 border border-border rounded-lg">
+            <TableInputContainer events={data} />
+          </div>
         </div>
       </div>
       <footer className="text-center text-sm text-muted-foreground mx-auto rounded-full px-4 py-2 border border-border backdrop-blur-[2px]">
