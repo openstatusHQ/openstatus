e581fb920e9839d20cb7f924c8c287bdbaf7192d
mxkaske <maximilian@kaske.org>
2023-06-28 23:19:45 +0200

    fix: overflow issue

--- apps/web/src/app/_components/event-table.tsx
+++ apps/web/src/app/_components/event-table.tsx
@@ -30,69 +30,71 @@ export function EventTable({
   const [open, toggle] = React.useReducer((state) => !state, false);
   return (
     <div className="relative overflow-hidden max-h-56">
-      <Table>
-        <TableCaption>A list of the latest pings.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Time</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Latency</TableHead>
-            <TableHead className="text-right">URL</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {events.map((event) => {
-            const isOk = event.statusCode === 200;
-            const url = new URL(event.url);
-            return (
-              <TableRow key={event.timestamp}>
-                <TableCell className="font-medium">
-                  {formatDistance(new Date(event.timestamp), new Date(), {
-                    addSuffix: true,
-                    includeSeconds: true,
-                  })}
-                </TableCell>
-                <TableCell>
-                  <Badge
-                    variant="outline"
-                    className={cn(
-                      "text-xs py-0.5 px-2",
-                      isOk
-                        ? "border-green-100 bg-green-50"
-                        : "border-red-100 bg-red-50"
-                    )}
-                  >
-                    {event.statusCode}
-                    <div
+      <div className="relative overflow-y-scroll max-h-56">
+        <Table>
+          <TableCaption>
+            A list of the latest {events.length} pings.
+          </TableCaption>
+          <TableHeader>
+            <TableRow className="sticky top-0">
+              <TableHead>Time</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Latency</TableHead>
+              <TableHead className="text-right">URL</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {events.map((event) => {
+              const isOk = event.statusCode === 200;
+              const url = new URL(event.url);
+              return (
+                <TableRow key={event.timestamp}>
+                  <TableCell className="font-medium">
+                    {formatDistance(new Date(event.timestamp), new Date(), {
+                      addSuffix: true,
+                      includeSeconds: true,
+                    })}
+                  </TableCell>
+                  <TableCell>
+                    <Badge
+                      variant="outline"
                       className={cn(
-                        "rounded-full bg-foreground h-1.5 w-1.5 ml-1",
-                        isOk ? "bg-green-500" : "bg-red-500"
+                        "text-xs py-0.5 px-2",
+                        isOk
+                          ? "border-green-100 bg-green-50"
+                          : "border-red-100 bg-red-50"
                       )}
-                    />
-                  </Badge>
-                </TableCell>
-                <TableCell className="text-muted-foreground">
-                  {event.latency}
-                </TableCell>
-                <TableCell className="text-right font-light truncate">
-                  {url.pathname}
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+                    >
+                      {event.statusCode}
+                      <div
+                        className={cn(
+                          "rounded-full bg-foreground h-1.5 w-1.5 ml-1",
+                          isOk ? "bg-green-500" : "bg-red-500"
+                        )}
+                      />
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="text-muted-foreground">
+                    {event.latency}
+                  </TableCell>
+                  <TableCell className="text-right font-light truncate">
+                    {url.pathname}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
       {!open && (
         <div className="absolute inset-0 flex items-end justify-center bg-gradient bg-gradient-to-b from-transparent from-20% to-background">
-          {/* TODO: enable button */}
           <Button
             onClick={toggle}
             variant="outline"
             size="sm"
             className="rounded-full backdrop-blur-sm"
-            // disabled
           >
-            {`A total of ${events.length} events.`}
+            A total of {events.length} events.
           </Button>
         </div>
       )}
