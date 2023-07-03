99a09944d1d4c59d29bf8d9ebf03233fa25dda99
mxkaske <maximilian@kaske.org>
2023-07-03 20:24:37 +0200

    fix: missing param

--- apps/web/src/lib/tb.ts
+++ apps/web/src/lib/tb.ts
@@ -1,3 +1,4 @@
+import type { Region } from "@openstatus/tinybird";
 import { getResponseList, Tinybird } from "@openstatus/tinybird";
 
 import { env } from "@/env.mjs";
@@ -6,10 +7,16 @@ import MOCK from "@/mock/response-list.json";
 const tb = new Tinybird({ token: env.TINY_BIRD_API_KEY });
 
 // rename if needed - too generic
-export async function getData({ siteId }: { siteId?: string }) {
+export async function getData({
+  siteId,
+  region,
+}: {
+  siteId?: string;
+  region?: Region;
+}) {
   let data = MOCK;
   if (process.env.NODE_ENV !== "development") {
-    const res = await getResponseList(tb)({ siteId });
+    const res = await getResponseList(tb)({ siteId, region });
     data = res.data;
   }
   return data;
