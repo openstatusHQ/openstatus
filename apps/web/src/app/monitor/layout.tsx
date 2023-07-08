53d9c6f85335e21166a2511f4ed05d99535aa08b
mxkaske <maximilian@kaske.org>
2023-07-08 21:26:00 +0200

    chore: create back button

--- apps/web/src/app/monitor/layout.tsx
+++ apps/web/src/app/monitor/layout.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import Link from "next/link";
-import { ChevronLeft } from "lucide-react";
 
+import { BackButton } from "@/components/BackButton";
 import { Footer } from "@/components/layout/footer";
-import { Button } from "@/components/ui/button";
 
 // same layout as /play
 export default function MonitorLayout({
@@ -14,12 +12,7 @@ export default function MonitorLayout({
   return (
     <main className="container mx-auto flex min-h-screen w-full flex-col items-center justify-center space-y-6 p-4 md:p-8">
       <div className="z-10 flex w-full flex-1 flex-col items-start justify-center">
-        <Button asChild variant="link" className="group mb-1">
-          <Link href="/">
-            <ChevronLeft className="text-muted-foreground group-hover:text-foreground mr-1 h-4 w-4" />{" "}
-            Back
-          </Link>
-        </Button>
+        <BackButton />
         <div className="border-border w-full rounded-lg border p-3 backdrop-blur-[2px] md:p-6">
           {children}
         </div>
---
+++ apps/web/src/components/BackButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ChevronLeft } from "lucide-react";
+
+import { Button } from "./ui/button";
+
+export const BackButton = () => {
+  const router = useRouter();
+  return (
+    <Button variant="link" className="group mb-1" onClick={router.back}>
+      <ChevronLeft className="text-muted-foreground group-hover:text-foreground mr-1 h-4 w-4" />{" "}
+      Back
+    </Button>
+  );
+};
