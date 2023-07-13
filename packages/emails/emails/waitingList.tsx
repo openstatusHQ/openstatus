25b6b44bd631a0b27e1023635a35b9141169936c
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-07-13 18:08:59 +0200

    💌 send emails when user subscribe

---
+++ packages/emails/emails/waitingList.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { Body, Head, Html, Link, Preview } from "@react-email/components";
+
+const WaitingList = () => {
+  return (
+    <Html>
+      <Head>
+        <title>Thanks for joining OpenStatus waiting list</title>
+        <Preview>
+          Thanks for joining OpenStatus waiting list waiting list
+        </Preview>
+        <Body>
+          Hello,
+          <br />
+          <br />
+          We are working hard to get you access to our app. You can follow our
+          progress on our{" "}
+          <Link href="https://github.com/mxkaske/openstatus">
+            Github repository
+          </Link>
+          .
+          <br />
+          <br />
+          If you have any questions, I will be happy to answer them.
+          <br />
+          <br />
+          Thank you
+          <br />
+          Thibault Le Ouay Ducasse
+        </Body>
+      </Head>
+    </Html>
+  );
+};
+
+export default WaitingList;
---
+++ packages/emails/index.ts
@@ -0,0 +1,5 @@
+import { validateEmailNotDisposable } from "./emails/utils/utils";
+import WaitingList from "./emails/waitingList";
+import WelcomeEmail from "./emails/welcome";
+
+export { WelcomeEmail, WaitingList, validateEmailNotDisposable };
