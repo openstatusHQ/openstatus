74b6eede2bf7f66e36890e0d383e9f1a7b1c6749
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-07-25 22:45:25 +0200

    feat: seo for status page

--- apps/web/src/app/status-page/[domain]/page.tsx
+++ apps/web/src/app/status-page/[domain]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import { Header } from "@/components/dashboard/header";
@@ -5,7 +6,12 @@ import { Shell } from "@/components/dashboard/shell";
 import { MonitorList } from "@/components/status-page/monitor-list";
 import { api } from "@/trpc/server";
 
-export default async function Page({ params }: { params: { domain: string } }) {
+type Props = {
+  params: { domain: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function Page({ params }: Props) {
   // We should fetch the the monitors and incident here
   // also the page information
   if (!params.domain) return notFound();
@@ -26,4 +32,21 @@ export default async function Page({ params }: { params: { domain: string } }) {
       </div>
     </Shell>
   );
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const page = await api.page.getPageBySlug.query({ slug: params.domain });
+
+  return {
+    title: page?.title,
+    description: page?.description,
+    twitter: {
+      title: page?.title,
+      description: page?.description,
+    },
+    openGraph: {
+      title: page?.title,
+      description: page?.description,
+    },
+  };
 }
