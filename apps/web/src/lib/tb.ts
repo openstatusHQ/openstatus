e467d8cabd2868e7efe973977ce697429ba11444
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-07-27 19:22:16 +0200

    feat:  emails (#93)

--- apps/web/src/lib/tb.ts
+++ apps/web/src/lib/tb.ts
@@ -8,7 +8,7 @@ import {
   Tinybird,
 } from "@openstatus/tinybird";
 
-import { env } from "@/env.mjs";
+import { env } from "@/env";
 
 const tb = new Tinybird({ token: env.TINY_BIRD_API_KEY });
 
