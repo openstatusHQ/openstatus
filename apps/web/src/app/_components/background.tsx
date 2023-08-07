cc2bc450c613123bb812a15a96e8b5a64fff0ef6
Kartik Khorwal <hellokartikk@gmail.com>
2023-08-08 02:29:46 +0530

    fix: empty-screen

--- apps/web/src/app/_components/background.tsx
+++ apps/web/src/app/_components/background.tsx
@@ -13,32 +13,35 @@ export default function Background({
   useMouseMove();
   return (
     <>
-      <div className="sticky top-0 left-0 -z-50 h-screen w-screen overflow-hidden">
-        <div className="bg-muted-foreground/20 absolute inset-0 z-[-1]" />
-        <div className="bg-gradient-radial from-muted-foreground/80 absolute left-[--x] top-[--y] z-[-1] h-56 w-56 -translate-x-1/2 -translate-y-1/2 rounded-full from-0% to-transparent to-90% blur-md" />
-        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-          <defs>
-            <pattern
-              id="dotted-pattern"
-              width="16"
-              height="16"
-              patternUnits="userSpaceOnUse"
-            >
-              <circle cx="2" cy="2" r="1" fill="black" />
-            </pattern>
-            <mask id="dots-mask">
-              <rect width="100%" height="100%" fill="white" />
-              <rect width="100%" height="100%" fill="url(#dotted-pattern)" />
-            </mask>
-          </defs>
-          <rect
-            width="100%"
-            height="100%"
-            fill="hsl(var(--background))"
-            mask="url(#dots-mask)"
-          />
-        </svg>
+      <div className="fixed top-0 left-0 -z-50">
+        <div className="sticky top-0 left-0 -z-50 h-screen w-screen overflow-hidden">
+          <div className="bg-muted-foreground/20 absolute inset-0 z-[-1]" />
+          <div className="bg-gradient-radial from-muted-foreground/80 absolute left-[--x] top-[--y] z-[-1] h-56 w-56 -translate-x-1/2 -translate-y-1/2 rounded-full from-0% to-transparent to-90% blur-md" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+            <defs>
+              <pattern
+                id="dotted-pattern"
+                width="16"
+                height="16"
+                patternUnits="userSpaceOnUse"
+              >
+                <circle cx="2" cy="2" r="1" fill="black" />
+              </pattern>
+              <mask id="dots-mask">
+                <rect width="100%" height="100%" fill="white" />
+                <rect width="100%" height="100%" fill="url(#dotted-pattern)" />
+              </mask>
+            </defs>
+            <rect
+              width="100%"
+              height="100%"
+              fill="hsl(var(--background))"
+              mask="url(#dots-mask)"
+            />
+          </svg>
+        </div>
       </div>
+
       {children}
     </>
   );
