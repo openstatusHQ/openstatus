51fc9204cef0e90bbe1a143aec40ae6d70d7aec5
Kartik Khorwal <hellokartikk@gmail.com>
2023-08-08 02:42:36 +0530

    update: removed not required z-index class

--- apps/web/src/app/_components/background.tsx
+++ apps/web/src/app/_components/background.tsx
@@ -14,7 +14,7 @@ export default function Background({
   return (
     <>
       <div className="fixed top-0 left-0 -z-50">
-        <div className="sticky top-0 left-0 -z-50 h-screen w-screen overflow-hidden">
+        <div className="sticky top-0 left-0 h-screen w-screen overflow-hidden">
           <div className="bg-muted-foreground/20 absolute inset-0 z-[-1]" />
           <div className="bg-gradient-radial from-muted-foreground/80 absolute left-[--x] top-[--y] z-[-1] h-56 w-56 -translate-x-1/2 -translate-y-1/2 rounded-full from-0% to-transparent to-90% blur-md" />
           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
