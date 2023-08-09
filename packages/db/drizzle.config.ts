9ca5f730949d809405263c908f2e2f24c524b6f5
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-08-09 11:42:10 +0200

    💌 store user email (#167)
    * 💌 store user email
    
    * 🚀 add photo  url

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import type { Config } from "drizzle-kit";
 
-import { env } from "./env";
+import { env } from "./env.mjs";
 
 export default {
   schema: "./src/schema/index.ts",
---
+++ packages/db/drizzle/0002_luxuriant_ser_duncan.sql
@@ -0,0 +1,4 @@
+ALTER TABLE user ADD `first_name` text DEFAULT '';--> statement-breakpoint
+ALTER TABLE user ADD `last_name` text DEFAULT '';--> statement-breakpoint
+ALTER TABLE user ADD `email` text DEFAULT '';--> statement-breakpoint
+ALTER TABLE user ADD `photo_url` text DEFAULT '';
