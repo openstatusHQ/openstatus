3009b055e6c361aee2f700c997b6ed2ad7cfca33
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-08-12 16:15:00 +0200

    👨‍⚖️ Some boring legal info (#176)
    * 👨‍⚖️
    
    * 🐛
    
    * ✏️
    
    * 💅 add layout

--- apps/web/contentlayer.config.ts
+++ apps/web/contentlayer.config.ts
@@ -2,13 +2,14 @@ import { makeSource } from "contentlayer/source-files";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
+import { LegalPost } from "./src/contentlayer/documents/legal";
 import { Post } from "./src/contentlayer/documents/post";
 import autolinkHeadings from "./src/contentlayer/plugins/autolink-headings";
 import prettyCode from "./src/contentlayer/plugins/pretty-code";
 
 export default makeSource({
   contentDirPath: "src/content/",
-  documentTypes: [Post],
+  documentTypes: [Post, LegalPost],
   mdx: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypeSlug, prettyCode, autolinkHeadings],
