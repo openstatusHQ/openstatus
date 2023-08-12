4a6d44b9d20348d47179c7f77d22888667c64073
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-08-12 16:32:37 +0200

    🗑️

--- packages/db/drizzle/0003_mean_shatterstar.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE incident ADD `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-ALTER TABLE incident_update ADD `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-ALTER TABLE page ADD `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-ALTER TABLE monitor ADD `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-ALTER TABLE user ADD `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-ALTER TABLE workspace ADD `created_at` integer DEFAULT (strftime('%s', 'now'));
