759b92a0533e668805c09a1f8aeac776675f1750
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-08-13 13:27:36 +0200

    🗑️

--- packages/db/drizzle/0003_free_black_panther.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE workspace ADD `subscription_id` text;--> statement-breakpoint
-ALTER TABLE workspace ADD `plan` text(3) DEFAULT 'hobby';--> statement-breakpoint
-ALTER TABLE workspace ADD `ends_at` integer;--> statement-breakpoint
-ALTER TABLE workspace ADD `paid_until` integer;
