662c2317c13468efb282b4355b2fb23eb77affb2
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-08-18 23:23:21 +0300

    appending open-status header to avoid tracking requests if this header exist

--- apps/web/src/app/api/ping/route.ts
+++ apps/web/src/app/api/ping/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function GET(req:Request) {
+  req.headers.set('OpenStatus-Ping',true)
   return NextResponse.json({ ping: "pong" }, { status: 200 });
 }
