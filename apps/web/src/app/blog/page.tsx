f1076db4a33f6d890a05667bb5445655fd6ee8d0
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-08-20 17:47:56 +0200

    ✏️

--- apps/web/src/app/blog/page.tsx
+++ apps/web/src/app/blog/page.tsx
@@ -14,23 +14,29 @@ export default async function Post() {
     <Shell>
       <div className="grid gap-8">
         <h1 className="text-foreground font-cal text-4xl">Blog</h1>
-        <div>
+        <div className="space-y-4">
           {posts.map((post) => (
-            <Link href={`/blog/${post.slug}`} key={post.slug}>
-              <section>
-                <p className="text-foreground font-cal text-2xl">
-                  {post.title}
-                </p>
-                <p className="text-muted-foreground">{post.description}</p>
-                <p className="text-muted-foreground mt-1 text-xs">
-                  {post.author.name}
-                  <span className="text-muted-foreground/70 mx-1">&bull;</span>
-                  {formatDate(new Date(post.publishedAt))}
-                  <span className="text-muted-foreground/70 mx-1">&bull;</span>
-                  {post.readingTime}
-                </p>
-              </section>
-            </Link>
+            <div key={post.slug}>
+              <Link href={`/blog/${post.slug}`}>
+                <section>
+                  <p className="text-foreground font-cal text-2xl">
+                    {post.title}
+                  </p>
+                  <p className="text-muted-foreground">{post.description}</p>
+                  <p className="text-muted-foreground mt-1 text-xs">
+                    {post.author.name}
+                    <span className="text-muted-foreground/70 mx-1">
+                      &bull;
+                    </span>
+                    {formatDate(new Date(post.publishedAt))}
+                    <span className="text-muted-foreground/70 mx-1">
+                      &bull;
+                    </span>
+                    {post.readingTime}
+                  </p>
+                </section>
+              </Link>
+            </div>
           ))}
         </div>
       </div>
