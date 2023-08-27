0d2ee6ce6244cf27d43e60ef4f6900d14f13d241
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-08-27 21:28:01 +0000

    feat: add open graph content and update page title (#237)
    * feat: add open graph content and update page title
    
    * Update theme.config.tsx
    
    Co-authored-by: Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>
    
    * add description and twitter to OG
    
    * add page title to NextSeo
    
    ---------
    
    Co-authored-by: Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -13,6 +13,22 @@ const config: DocsThemeConfig = {
   footer: {
     text: "OpenStatus Documentation",
   },
+  head: (
+    <>
+      <title>OpenStatus Documentation</title>
+      <meta
+        name="description"
+        content="An Open Source alternative for your next Status Page"
+      />
+      <meta property="og:image" content="https://openstatus.dev/api/og" />
+    </>
+  ),
+  useNextSeoProps() {
+    return {
+      title: "OpenStatus Documentation",
+      titleTemplate: "%s | OpenStatus Documentation",
+    };
+  },
 };
 
 export default config;
