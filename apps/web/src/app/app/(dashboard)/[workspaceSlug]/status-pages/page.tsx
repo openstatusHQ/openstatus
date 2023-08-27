dc62ef9df4116206543c4cd8386cf5df4cb4ec9a
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-08-27 10:22:52 +0000

    add an `s` to page to make it plural (#243)

--- apps/web/src/app/app/(dashboard)/[workspaceSlug]/status-pages/page.tsx
+++ apps/web/src/app/app/(dashboard)/[workspaceSlug]/status-pages/page.tsx
@@ -42,7 +42,7 @@ export default async function Page({
     <div className="grid gap-6 md:grid-cols-2 md:gap-8">
       <Header
         title="Status Page"
-        description="Overview of all your status page."
+        description="Overview of all your status pages."
       >
         <Button asChild={!disableButton} disabled={disableButton}>
           <Link href="./status-pages/edit">Create</Link>
