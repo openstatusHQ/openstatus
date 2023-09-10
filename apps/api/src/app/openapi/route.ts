380c3e324994f9e9e5d68038f75eb1bda3f2b6d4
Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>
2023-09-10 18:23:26 +0200

    fix: generate swagger.json on build (#293)

--- apps/api/src/app/openapi/route.ts
+++
@@ -1,8 +0,0 @@
-import { getApiDocs } from "@/lib/swagger";
-
-export const dynamic = "force-dynamic";
-
-export async function GET(_: Request) {
-  const data = await getApiDocs();
-  return new Response(JSON.stringify(data), { status: 200 });
-}
