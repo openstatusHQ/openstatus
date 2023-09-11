702bf35a4550b67deeb9a7d8d683145c9aad6696
mxkaske <maximilian@kaske.org>
2023-09-11 18:53:29 +0200

    wip:

---
+++ apps/rest-api/src/index.ts
@@ -0,0 +1,31 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+import "dotenv/config";
+
+import { middleware } from "./middleware";
+import { DELETE, GET, POST, PUT } from "./monitors";
+
+/**
+ * Base Path "/v1" for our api
+ */
+const app = new Hono().basePath("/v1");
+
+/**
+ * Authentification Middleware
+ */
+app.use("*", middleware);
+
+/**
+ * REST API for monitors
+ */
+const monitors = new Hono();
+
+monitors.get("/:id", GET);
+monitors.post("/", POST);
+monitors.put("/:id", PUT);
+monitors.delete("/:id", DELETE);
+
+app.route("/monitors", monitors);
+
+serve(app);
---
+++ apps/rest-api/src/middleware.ts
@@ -0,0 +1,23 @@
+import { Unkey } from "@unkey/api";
+import { Context, Env, Next } from "hono";
+
+const unkey = new Unkey({ token: process.env.UNKEY_TOKEN! });
+
+export async function middleware(c: Context<Env, "/v1/*", {}>, next: Next) {
+  const auth = c.req.header("x-openstatus-key");
+
+  if (!auth) return c.text("Unauthorized", 401);
+
+  if (process.env.NODE_ENV === "production") {
+    const { error, result } = await unkey.keys.verify({ key: auth });
+
+    if (error) return c.text("Bad Request", 400);
+
+    if (!result.valid) return c.text("Unauthorized", 401);
+
+    c.req.raw.headers.append("x-workspace-id", `${result.ownerId}`);
+  } else {
+    c.req.raw.headers.append("x-workspace-id", "1");
+  }
+  await next();
+}
