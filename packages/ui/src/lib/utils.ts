5c377d11a9790e7446f9a9493ecb68b3c12cad27
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-09-19 21:36:37 +0200

    😁

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -5,4 +5,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
