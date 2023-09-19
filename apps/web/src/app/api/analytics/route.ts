74e637e7da60722ed400fd5cc7f51ec647b8885b
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-09-19 20:38:34 +0200

    🥅

--- apps/web/src/app/api/analytics/route.ts
+++ apps/web/src/app/api/analytics/route.ts
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs";
 
 import { analytics, trackAnalytics } from "@openstatus/analytics";
+import { db, eq } from "@openstatus/db";
+import { user } from "@openstatus/db/src/schema";
 
 /**
  * Simple Jitsu Event
@@ -8,8 +10,11 @@ import { analytics, trackAnalytics } from "@openstatus/analytics";
 export async function GET() {
   const { userId } = auth();
 
-  await analytics.identify(userId, {
-    userId: userId,
+  const data = await db.select().from(user).where(eq(user.tenantId, String(userId))).get();
+
+  await analytics.identify(data?.id, {
+    userId: data?.id,
+    email: data?.email,
   });
   await trackAnalytics({
     event: "User Vercel Beta",
