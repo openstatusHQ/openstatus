bff8b019fce6fa82bcd763759672dcc5c6cdc31c
mxkaske <maximilian@kaske.org>
2023-09-22 16:12:29 +0200

    feat: add partners

--- apps/web/src/components/marketing/partners.tsx
+++ apps/web/src/components/marketing/partners.tsx
@@ -2,26 +2,40 @@ import { ArrowUpRight } from "lucide-react";
 
 export function Partners() {
   return (
-    <div className="flex flex-wrap items-end gap-8 px-3 md:mx-6">
-      <a
-        href="https://status.hanko.io"
-        target="_blank"
-        rel="noreferrer"
-        className="group inline-flex items-center justify-center underline underline-offset-4 hover:no-underline"
-      >
-        Hanko
-        <ArrowUpRight className="text-muted-foreground group-hover:text-foreground ml-1 h-4 w-4 flex-shrink-0" />
-      </a>
-      <a
-        href="https://status.documenso.com"
-        target="_blank"
-        rel="noreferrer"
-        className="group inline-flex items-center justify-center underline underline-offset-4 hover:no-underline"
-      >
-        Documenso
-        <ArrowUpRight className="text-muted-foreground group-hover:text-foreground ml-1 h-4 w-4 flex-shrink-0" />
-      </a>
-      <p className="text-muted-foreground text-xs">Your logo</p>
+    <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 sm:gap-16">
+      <div className="flex items-center justify-center">
+        <a
+          href="https://status.hanko.io"
+          target="_blank"
+          rel="noreferrer"
+          className="group inline-flex items-center justify-center underline underline-offset-4 hover:no-underline"
+        >
+          <img
+            src="/assets/partners/hanko.svg"
+            alt="hanko"
+            className="h-8 w-16 object-contain"
+          />
+          <ArrowUpRight className="text-muted-foreground group-hover:text-foreground ml-1 h-4 w-4 flex-shrink-0" />
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a
+          href="https://status.documenso.com"
+          target="_blank"
+          rel="noreferrer"
+          className="group inline-flex items-center justify-center underline underline-offset-4 hover:no-underline"
+        >
+          <img
+            src="/assets/partners/documenso.svg"
+            alt="hanko"
+            className="h-8 w-32 object-contain"
+          />
+          <ArrowUpRight className="text-muted-foreground group-hover:text-foreground ml-1 h-4 w-4 flex-shrink-0" />
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <p className="text-muted-foreground text-xs">You</p>
+      </div>
     </div>
   );
 }
