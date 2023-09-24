1075e98c2053f48585eca85dc4c7e4ab73f45b9f
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-09-24 18:09:48 +0530

    status filter ui component update

--- apps/web/src/components/data-table/data-table-toolbar.tsx
+++ apps/web/src/components/data-table/data-table-toolbar.tsx
@@ -4,9 +4,11 @@ import type { Table } from "@tanstack/react-table";
 import { X } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { codesDict } from "@/data/code-dictionary";
 import { regionsDict } from "@/data/regions-dictionary";
 import { DataTableDateRangePicker } from "./data-table-date-ranger-picker";
 import { DataTableFacetedFilter } from "./data-table-faceted-filter";
+import { DataTableFilterDropDown } from "./data-table-filter-dropdown";
 import { DataTableFilterInput } from "./data-table-filter-input";
 
 interface DataTableToolbarProps<TData> {
@@ -21,32 +23,43 @@ export function DataTableToolbar<TData>({
   return (
     <div className="flex flex-wrap items-center justify-between gap-3">
       <div className="flex flex-1 items-center gap-2">
-        <DataTableFilterInput table={table} />
-        {table.getColumn("region") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("region")}
-            title="Region"
-            options={Object.keys(regionsDict).map((key) => {
-              const typedKey = key as keyof typeof regionsDict;
-              return {
-                label: regionsDict[typedKey].location,
-                value: regionsDict[typedKey].code,
-              };
-            })}
-          />
-        )}
-        {isFiltered && (
-          <Button
-            variant="ghost"
-            onClick={() => table.resetColumnFilters()}
-            className="h-8 px-2 lg:px-3"
-          >
-            Reset
-            <X className="ml-2 h-4 w-4" />
-          </Button>
-        )}
-      </div>
-      <DataTableDateRangePicker />
+        {table.getColumn("statusCode") && (
+      <DataTableFilterDropDown
+        column={table.getColumn("statusCode")}
+        title="Filter by Status Code"
+        options={Object.keys(codesDict).map((key) => {
+            const typedKey = key as keyof typeof codesDict;
+            return {
+              label: codesDict[typedKey].status_code,
+              value: codesDict[typedKey].code,
+            };
+          })}
+       />
+     )}
+      {table.getColumn("region") && (
+        <DataTableFacetedFilter
+          column={table.getColumn("region")}
+          title="Region"
+          options={Object.keys(regionsDict).map((key) => {
+            const typedKey = key as keyof typeof regionsDict;
+            return {
+              label: regionsDict[typedKey].location,
+              value: regionsDict[typedKey].code,
+            };
+          })}
+        />
+      )}
+      {isFiltered && (
+        <Button
+          variant="ghost"
+          onClick={() => table.resetColumnFilters()}
+          className="h-8 px-2 lg:px-3"
+        >
+          Reset
+          <X className="ml-2 h-4 w-4" />
+        </Button>
+      )}
     </div>
-  );
-}
+    <DataTableDateRangePicker />
+  </div>
+);}
---
+++ apps/web/src/data/code-dictionary.ts
@@ -0,0 +1,26 @@
+export const codesDict = {
+    
+    
+    arn1: {
+      code: "200",
+      name: "OK",
+      status_code: "200",
+    },
+    bom1: {
+      code: "500",
+      name: "Internal Server Error",
+      status_code: "500",
+    },
+    cdg1: {
+      code: "404",
+      name: "Not Found",
+      status_code: "404",
+    },
+    cle1: {
+      code: "429",
+      name: "Too Many Requests",
+      status_code: "429",
+    },
+   
+  } as const;
+  
