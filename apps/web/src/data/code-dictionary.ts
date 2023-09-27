c43f85b5b91132660bc2d4a3aa6a9b83714667c1
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-09-27 16:02:57 +0530

    DataTableFacetedFilter updated

--- apps/web/src/data/code-dictionary.ts
+++ apps/web/src/data/code-dictionary.ts
@@ -1,26 +1,22 @@
 export const codesDict = {
-    
-    
-    arn1: {
-      code: "200",
-      name: "OK",
-      status_code: "200",
-    },
-    bom1: {
-      code: "500",
-      name: "Internal Server Error",
-      status_code: "500",
-    },
-    cdg1: {
-      code: "404",
-      name: "Not Found",
-      status_code: "404",
-    },
-    cle1: {
-      code: "429",
-      name: "Too Many Requests",
-      status_code: "429",
-    },
-   
-  } as const;
-  
+  "200": {
+    code:"200",
+    name: "OK",
+    status_code: "200",
+  },
+  "500": {
+    code:"200",
+    name: "Internal Server Error",
+    status_code: "500",
+  },
+  "404": {
+    code:"200",
+    name: "Not Found",
+    status_code: "404",
+  },
+  "429": {
+    code:"200",
+    name: "Too Many Requests",
+    status_code: "429",
+  },
+} as const;
