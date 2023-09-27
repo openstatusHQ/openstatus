d9682478bf93c053c46527634fe5a35db9752d94
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-09-27 09:09:36 +0200

    🚧

--- packages/notifications/email/src/index.ts
+++ packages/notifications/email/src/index.ts
@@ -1,12 +1,12 @@
-import { render, renderAsync } from "@react-email/render";
+import { renderAsync } from "@react-email/render";
 import type { z } from "zod";
 
 import type {
   basicMonitorSchema,
   selectNotificationSchema,
 } from "@openstatus/db/src/schema";
-import { Alert, resend } from "@openstatus/emails";
 
+import { env } from "../env";
 import { EmailConfigurationSchema } from "./schema/config";
 
 export const send = async ({
@@ -17,23 +17,26 @@ export const send = async ({
   notification: z.infer<typeof selectNotificationSchema>;
 }) => {
   const config = EmailConfigurationSchema.parse(notification.data);
-
-  const html = render(
-    Alert({
-      data: {
-        monitorName: monitor.name,
-        monitorUrl: monitor.url,
-        recipientName: config.name,
-      },
-    }),
-  );
-
   const { to } = config;
 
-  await resend.emails.send({
-    to,
-    from: "Notifications <ping@openstatus.dev>",
-    subject: "Your monitor is down 🚨 ",
-    html,
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      to,
+      from: "Notifications <ping@openstatus.dev>",
+
+      subject: "Your monitor is down 🚨 ",
+      html: "<strong>it works!</strong>",
+    }),
   });
+
+  if (res.ok) {
+    const data = await res.json();
+    console.log(data);
+    // return NextResponse.json(data);
+  }
 };
