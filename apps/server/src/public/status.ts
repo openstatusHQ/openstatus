35cd7a2d8d2c368fbe0b5bf8023bc1621d650c90
mxkaske <maximilian@kaske.org>
2023-10-01 18:15:07 +0200

    chore: add caching

--- apps/server/src/public/status.ts
+++ apps/server/src/public/status.ts
@@ -3,10 +3,12 @@ import { Hono } from "hono";
 import { db, eq } from "@openstatus/db";
 import { monitor, monitorsToPages, page } from "@openstatus/db/src/schema";
 import { getMonitorList, Tinybird } from "@openstatus/tinybird";
+import { Redis } from "@openstatus/upstash";
 
 // TODO: include ratelimiting
 
 const tb = new Tinybird({ token: process.env.TINY_BIRD_API_KEY || "" });
+const redis = Redis.fromEnv();
 
 enum Status {
   Operational = "operational",
@@ -21,6 +23,9 @@ export const status = new Hono();
 
 status.get("/:slug", async (c) => {
   const { slug } = c.req.param();
+
+  const cache = await redis.get(slug);
+  if (cache) return c.json({ status: cache });
 
   // { monitors, pages, monitors_to_pages }
   const monitorData = await db
@@ -63,10 +68,17 @@ status.get("/:slug", async (c) => {
 
   const ratio = data.ok / data.count;
 
-  if (isNaN(ratio)) return c.json({ status: Status.Unknown });
-  if (ratio >= 0.98) return c.json({ status: Status.Operational });
-  if (ratio >= 0.6) return c.json({ status: Status.DegradedPerformance });
-  if (ratio >= 0.3) return c.json({ status: Status.PartialOutage });
-  if (ratio >= 0) return c.json({ status: Status.MajorOutage });
-  return c.json({ status: Status.Unknown });
+  const status = getStatus(ratio);
+  await redis.set(slug, status, { ex: 30 });
+
+  return c.json({ status });
 });
+
+function getStatus(ratio: number) {
+  if (isNaN(ratio)) return Status.Unknown;
+  if (ratio >= 0.98) return Status.Operational;
+  if (ratio >= 0.6) return Status.DegradedPerformance;
+  if (ratio >= 0.3) return Status.PartialOutage;
+  if (ratio >= 0) return Status.MajorOutage;
+  return Status.Unknown;
+}
