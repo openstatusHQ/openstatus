f1f014c74fc1fc4e7931beebe5bc28a7a807bc32
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-10-14 00:42:51 +0200

    🧪 fix tests

--- apps/server/src/checker/alerting.test.ts
+++ apps/server/src/checker/alerting.test.ts
@@ -3,7 +3,7 @@ import { expect, it, vi } from "vitest";
 import { triggerAlerting } from "./alerting";
 import * as utils from "./utils";
 
-it.only("should send email notification", async () => {
+it("should send email notification", async () => {
   vi.mock("utils");
   const mockedFn = vi.fn();
   utils.providerToFunction["email"] = mockedFn;
