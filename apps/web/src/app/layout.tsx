ce9ebae879525bd01a0df34c59764c44f515cccb
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-10-25 18:51:21 +0200

    🧹 remove vercel analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,7 +14,6 @@ import {
   twitterMetadata,
 } from "@/app/shared-metadata";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
-import { ClientAnalytics } from "./_components/analytics";
 import Background from "./_components/background";
 
 const inter = Inter({ subsets: ["latin"] });
