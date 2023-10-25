32aa34092ca3c47945101972eccfdab231ce7821
Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>
2023-10-25 21:38:08 +0200

    status check (#393)
    * wip
    
    * chore: filter include monitoring
    
    * fix: include

--- apps/web/src/lib/tracker.ts
+++ apps/web/src/lib/tracker.ts
@@ -1,17 +1,47 @@
 import type { Monitor } from "@openstatus/tinybird";
 
+export type StatusVariant = "up" | "degraded" | "down" | "empty";
+
+type GetStatusReturnType = {
+  label: string;
+  variant: StatusVariant;
+  twColor: string;
+  twBgColor: string;
+};
+
 /**
  * Get the status of a monitor based on its ratio
  * @param ratio
  * @returns
  */
-export const getStatus = (
-  ratio: number,
-): { label: string; variant: "up" | "degraded" | "down" | "empty" } => {
-  if (isNaN(ratio)) return { label: "Missing", variant: "empty" };
-  if (ratio >= 0.98) return { label: "Operational", variant: "up" };
-  if (ratio >= 0.5) return { label: "Degraded", variant: "degraded" };
-  return { label: "Downtime", variant: "down" };
+export const getStatus = (ratio: number): GetStatusReturnType => {
+  if (isNaN(ratio))
+    return {
+      label: "Missing",
+      variant: "empty",
+      twColor: "text-gray-500",
+      twBgColor: "bg-gray-500",
+    };
+  if (ratio >= 0.98)
+    return {
+      label: "Operational",
+      variant: "up",
+      twColor: "text-green-500",
+      twBgColor: "bg-green-500",
+    };
+  if (ratio >= 0.5)
+    return {
+      label: "Degraded",
+      variant: "degraded",
+      twColor: "text-yellow-500",
+      twBgColor: "bg-yellow-500",
+    };
+  return {
+    label: "Downtime",
+    variant: "down",
+    twColor: "text-red-500",
+    twBgColor: "bg-red-500",
+  };
 };
 
 /**
