5d7e61fa5edb3026d63ac3e9f9ee267f3f97cd98
mxkaske <maximilian@kaske.org>
2023-10-30 20:00:07 +0100

    revert: change

--- apps/web/src/app/_components/input-search.tsx
+++ apps/web/src/app/_components/input-search.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import type { Command as CommandPrimitive } from "cmdk";
-import { useCommandState } from "cmdk";
+import { Command as CommandPrimitive, useCommandState } from "cmdk";
 
 import type { Ping } from "@openstatus/tinybird";
 import {
@@ -68,121 +67,119 @@ export function InputSearch({
 
   type SearchKey = keyof typeof search;
 
-  return null; // FIXME: vercel deployment
+  return (
+    <Command
+      className="overflow-visible bg-transparent"
+      filter={(value) => {
+        if (value.includes(currentWord.toLowerCase())) return 1;
+        return 0;
+      }}
+    >
+      <CommandPrimitive.Input
+        ref={inputRef}
+        value={inputValue}
+        onValueChange={setInputValue}
+        onKeyDown={(e) => {
+          if (e.key === "Escape") inputRef?.current?.blur();
+        }}
+        onBlur={() => setOpen(false)}
+        onFocus={() => setOpen(true)}
+        onInput={(e) => {
+          const caretPositionStart = e.currentTarget?.selectionStart || -1;
+          const inputValue = e.currentTarget?.value || "";
 
-  // return (
-  //   <Command
-  //     className="overflow-visible bg-transparent"
-  //     filter={(value) => {
-  //       if (value.includes(currentWord.toLowerCase())) return 1;
-  //       return 0;
-  //     }}
-  //   >
-  //     <CommandPrimitive.Input
-  //       ref={inputRef}
-  //       value={inputValue}
-  //       onValueChange={setInputValue}
-  //       onKeyDown={(e) => {
-  //         if (e.key === "Escape") inputRef?.current?.blur();
-  //       }}
-  //       onBlur={() => setOpen(false)}
-  //       onFocus={() => setOpen(true)}
-  //       onInput={(e) => {
-  //         const caretPositionStart = e.currentTarget?.selectionStart || -1;
-  //         const inputValue = e.currentTarget?.value || "";
+          let start = caretPositionStart;
+          let end = caretPositionStart;
 
-  //         let start = caretPositionStart;
-  //         let end = caretPositionStart;
+          while (start > 0 && inputValue[start - 1] !== " ") {
+            start--;
+          }
+          while (end < inputValue.length && inputValue[end] !== " ") {
+            end++;
+          }
 
-  //         while (start > 0 && inputValue[start - 1] !== " ") {
-  //           start--;
-  //         }
-  //         while (end < inputValue.length && inputValue[end] !== " ") {
-  //           end++;
-  //         }
-
-  //         const word = inputValue.substring(start, end);
-  //         setCurrentWord(word);
-  //       }}
-  //       placeholder={`${events.length} total logs found...`}
-  //       className="border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex-1 rounded-md border bg-transparent px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-offset-2"
-  //     />
-  //     <div className="relative mt-2">
-  //       {open ? (
-  //         <div className="bg-popover text-popover-foreground animate-in absolute top-0 z-10 w-full rounded-md border shadow-md outline-none">
-  //           <CommandGroup className="max-h-64 overflow-auto">
-  //             {Object.keys(search).map((key) => {
-  //               if (
-  //                 inputValue.includes(`${key}:`) &&
-  //                 !currentWord.includes(`${key}:`)
-  //               )
-  //                 return null;
-  //               return (
-  //                 <React.Fragment key={key}>
-  //                   <CommandItem
-  //                     value={key}
-  //                     onMouseDown={(e) => {
-  //                       e.preventDefault();
-  //                       e.stopPropagation();
-  //                     }}
-  //                     onSelect={(value) => {
-  //                       setInputValue((prev) => {
-  //                         if (currentWord.trim() === "") {
-  //                           const input = `${prev}${value}`;
-  //                           return `${input}:`;
-  //                         }
-  //                         // lots of cheat
-  //                         const isStarting = currentWord === prev;
-  //                         const prefix = isStarting ? "" : " ";
-  //                         const input = prev.replace(
-  //                           `${prefix}${currentWord}`,
-  //                           `${prefix}${value}`,
-  //                         );
-  //                         return `${input}:`;
-  //                       });
-  //                       setCurrentWord(`${value}:`);
-  //                     }}
-  //                     className="group"
-  //                   >
-  //                     {key}
-  //                     <span className="text-muted-foreground/90 ml-1 hidden truncate group-aria-[selected=true]:block">
-  //                       {search[key as SearchKey]
-  //                         .map((str) => `[${str}]`)
-  //                         .join(" ")}
-  //                     </span>
-  //                   </CommandItem>
-  //                   {search[key as SearchKey].map((option) => {
-  //                     return (
-  //                       <SubItem
-  //                         key={option}
-  //                         value={`${key}:${option}`}
-  //                         onMouseDown={(e) => {
-  //                           e.preventDefault();
-  //                           e.stopPropagation();
-  //                         }}
-  //                         onSelect={(value) => {
-  //                           setInputValue((prev) => {
-  //                             const input = prev.replace(currentWord, value);
-  //                             return `${input.trim()} `;
-  //                           });
-  //                           setCurrentWord("");
-  //                         }}
-  //                         {...{ currentWord }}
-  //                       >
-  //                         {option}
-  //                       </SubItem>
-  //                     );
-  //                   })}
-  //                 </React.Fragment>
-  //               );
-  //             })}
-  //           </CommandGroup>
-  //           <CommandEmpty>No results found.</CommandEmpty>
-  //         </div>
-  //       ) : null}
-  //     </div>
-  //   </Command>
-  // );
+          const word = inputValue.substring(start, end);
+          setCurrentWord(word);
+        }}
+        placeholder={`${events.length} total logs found...`}
+        className="border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex-1 rounded-md border bg-transparent px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-offset-2"
+      />
+      <div className="relative mt-2">
+        {open ? (
+          <div className="bg-popover text-popover-foreground animate-in absolute top-0 z-10 w-full rounded-md border shadow-md outline-none">
+            <CommandGroup className="max-h-64 overflow-auto">
+              {Object.keys(search).map((key) => {
+                if (
+                  inputValue.includes(`${key}:`) &&
+                  !currentWord.includes(`${key}:`)
+                )
+                  return null;
+                return (
+                  <React.Fragment key={key}>
+                    <CommandItem
+                      value={key}
+                      onMouseDown={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                      }}
+                      onSelect={(value) => {
+                        setInputValue((prev) => {
+                          if (currentWord.trim() === "") {
+                            const input = `${prev}${value}`;
+                            return `${input}:`;
+                          }
+                          // lots of cheat
+                          const isStarting = currentWord === prev;
+                          const prefix = isStarting ? "" : " ";
+                          const input = prev.replace(
+                            `${prefix}${currentWord}`,
+                            `${prefix}${value}`,
+                          );
+                          return `${input}:`;
+                        });
+                        setCurrentWord(`${value}:`);
+                      }}
+                      className="group"
+                    >
+                      {key}
+                      <span className="text-muted-foreground/90 ml-1 hidden truncate group-aria-[selected=true]:block">
+                        {search[key as SearchKey]
+                          .map((str) => `[${str}]`)
+                          .join(" ")}
+                      </span>
+                    </CommandItem>
+                    {search[key as SearchKey].map((option) => {
+                      return (
+                        <SubItem
+                          key={option}
+                          value={`${key}:${option}`}
+                          onMouseDown={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                          }}
+                          onSelect={(value) => {
+                            setInputValue((prev) => {
+                              const input = prev.replace(currentWord, value);
+                              return `${input.trim()} `;
+                            });
+                            setCurrentWord("");
+                          }}
+                          {...{ currentWord }}
+                        >
+                          {option}
+                        </SubItem>
+                      );
+                    })}
+                  </React.Fragment>
+                );
+              })}
+            </CommandGroup>
+            <CommandEmpty>No results found.</CommandEmpty>
+          </div>
+        ) : null}
+      </div>
+    </Command>
+  );
 }
 
 interface SubItemProps
---
+++ apps/web/src/components/layout/breadcrumbs.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Fragment } from "react";
+import { usePathname } from "next/navigation";
+import { ChevronRight } from "lucide-react";
+
+// TODO: create place to put into layout.tsx
+
+export function Breadcrumbs() {
+  const pathname = usePathname();
+  const result = pathname.split("/").slice(3);
+
+  return (
+    <ul className="flex items-center">
+      {result.map((path, i) => {
+        return (
+          <Fragment key={i}>
+            <li>{path}</li>
+            {i !== result.length - 1 ? (
+              <ChevronRight className="text-muted-foreground mx-2 h-3 w-3" />
+            ) : null}
+          </Fragment>
+        );
+      })}
+    </ul>
+  );
+}
