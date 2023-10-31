c75c0a3f9aee1a96d2e49739f7330cb5009a1e35
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-10-31 23:40:27 +0100

    🚀

--- apps/web/src/components/layout/marketing-layout.tsx
+++ apps/web/src/components/layout/marketing-layout.tsx
@@ -1,14 +1,18 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { MarketingFooter } from "./marketing-footer";
 import { MarketingHeader } from "./marketing-header";
 
 export function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center space-y-6 p-4 md:p-8">
-      <MarketingHeader className="mx-auto w-full max-w-4xl" />
-      <div className="mx-auto flex w-full max-w-4xl flex-1 flex-col items-start justify-center">
-        {children}
-      </div>
-      <MarketingFooter className="mx-auto w-full max-w-4xl" />
-    </main>
+    <ClerkProvider>
+      <main className="flex min-h-screen w-full flex-col items-center justify-center space-y-6 p-4 md:p-8">
+        <MarketingHeader className="mx-auto w-full max-w-4xl" />
+        <div className="mx-auto flex w-full max-w-4xl flex-1 flex-col items-start justify-center">
+          {children}
+        </div>
+        <MarketingFooter className="mx-auto w-full max-w-4xl" />
+      </main>
+    </ClerkProvider>
   );
 }
