cfd78cbc2ba9068b8bc62c1a588627747a7477bd
mxkaske <maximilian@kaske.org>
2023-11-01 21:18:58 +0100

    fix: build

--- packages/ui/src/components/alert-dialog.tsx
+++ packages/ui/src/components/alert-dialog.tsx
@@ -11,10 +11,9 @@ const AlertDialog = AlertDialogPrimitive.Root;
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 
 const AlertDialogPortal = ({
-  className,
   ...props
 }: AlertDialogPrimitive.AlertDialogPortalProps) => (
-  <AlertDialogPrimitive.Portal className={cn(className)} {...props} />
+  <AlertDialogPrimitive.Portal {...props} />
 );
 AlertDialogPortal.displayName = AlertDialogPrimitive.Portal.displayName;
 
