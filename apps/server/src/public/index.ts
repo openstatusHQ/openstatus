3b19af2c9027e222dc757c9afee174f3b6544879
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-11-07 09:19:54 +0100

    🚀 improve fly

--- apps/server/src/public/index.ts
+++ apps/server/src/public/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 import { status } from "./status";
 
 export const publicRoute = new Hono();
+publicRoute.use("*", logger());
 
 publicRoute.route("/status", status);
