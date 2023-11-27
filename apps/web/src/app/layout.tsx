92f9b978556a041ccdfa8526e781cf28c8c51ada
mxkaske <maximilian@kaske.org>
2023-11-27 21:17:20 +0100

    chore: update default theme

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${calSans.variable}`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <Background>{children}</Background>
           <Toaster />
           <TailwindIndicator />
