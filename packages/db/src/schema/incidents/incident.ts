1d873b06b321a0081bf21a61ae7b7a21c2149899
Thibault Le Ouay <thibaultleouay@gmail.Com>
2023-12-06 18:28:01 +0100

    🔥 incident

--- packages/db/src/schema/incidents/incident.ts
+++ packages/db/src/schema/incidents/incident.ts
@@ -9,6 +9,8 @@ export const statusIncident = [
   "duplicated",
 ] as const;
 
+export const severityIncident = ["critical", "high", "medium", "low"] as const;
+
 export const incidentKind = ["incident", "test"] as const;
 
 export const incidentTable = sqliteTable("incident", {
@@ -16,16 +18,31 @@ export const incidentTable = sqliteTable("incident", {
   title: text("title").default("").notNull(),
   summary: text("summary").default("").notNull(),
   status: text("status", { enum: statusIncident }).default("triage").notNull(),
-  severity: text("severity").default("").notNull(),
+  severity: text("severity", { enum: severityIncident }).notNull(),
   incidentKind: text("incident_kind", { enum: incidentKind })
     .default("incident")
     .notNull(),
+
+  // Incident channel id can be discord, slack, etc
+  channelId: text("channel_id"),
+  channelName: text("channel_name"),
 
   // Incident creator email
   createdBy: text("created_by").notNull(),
 
   // Service affected by incident
   serviceId: integer("service_id"),
+
+  // This is based on the ICM (Incident Command Model)
+
+  // Commander of incident (optional) the id of the user
+  commanderId: integer("commander_id"),
+
+  // Communication Lead
+  communicationLeadId: integer("communication_lead_id"),
+
+  // Ops lead
+  opsLeadId: integer("ops_lead_id"),
 
   // Data related to incident timeline
   startedAt: integer("started_at", { mode: "timestamp" }),
