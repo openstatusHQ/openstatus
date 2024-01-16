727828c78fe715c6981486315329e087c7ced337
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-01-16 20:44:45 +0100

    🚀 fix test

--- apps/checker/pkg/tinybird/client_test.go
+++ apps/checker/pkg/tinybird/client_test.go
@@ -64,7 +64,7 @@ func TestSendEvent(t *testing.T) {
 			f: func(req *http.Request) (*http.Response, error) {
 				url = req.URL.String()
 				return &http.Response{
-					StatusCode: http.StatusOK,
+					StatusCode: http.StatusAccepted,
 				}, nil
 			},
 		}
@@ -73,6 +73,6 @@ func TestSendEvent(t *testing.T) {
 
 		err := client.SendEvent(ctx, "event")
 		require.NoError(t, err)
-		require.Equal(t, "https://api.tinybird.co/v0/events?name=ping_response__v5", url)
+		require.Equal(t, "https://api.tinybird.co/v0/events?name=ping_response__v6", url)
 	})
 }
