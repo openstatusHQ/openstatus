1789492fda889a6d41d3098c65ad189110c9a924
Kushagra <skushagra.sharma@gmail.com>
2024-01-30 14:26:21 +0530

    modified tests

--- apps/server/src/v1/incidents.test.ts
+++ apps/server/src/v1/incidents.test.ts
@@ -18,23 +18,3 @@ test("GET one Incident", async () => {
   });
 });
 
-test("create one Incident", async () => {
-  const res = await api.request("/incident", {
-    method: "POST",
-    headers: {
-      "x-openstatus-key": "1",
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      status: "investigating",
-      date: "2023-11-08T21:03:13.000Z",
-      title: "Test Incident",
-    }),
-  });
-  expect(res.status).toBe(200);
-  expect(await res.json()).toMatchObject({
-    id: expect.any(Number),
-    status: "investigating",
-    title: "Test Incident",
-  });
-});
