85cce1a357d23efff84213a778dfc416feac980f
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-02-01 18:17:58 +0100

    🥰 update about us

--- apps/web/src/app/about/_components/member.tsx
+++ apps/web/src/app/about/_components/member.tsx
@@ -1,21 +1,40 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import { cn } from "@openstatus/ui/src/lib/utils";
+
+import { Icons } from "@/components/icons";
+import type { ValidIcon } from "@/components/icons";
 
 export interface MemberProps {
   name: string;
   role: string;
   image: { src: string };
-  socials?: { label: string; href: string }[];
+  socials?: { label: string; href: string; icon: ValidIcon }[];
 }
 
 export function Member({ name, role, image, socials }: MemberProps) {
   return (
     <div className="grid w-full gap-3">
-      <div className="border-border relative aspect-square max-h-48 overflow-hidden rounded-lg border">
+      <div className="border-border relative aspect-square h-48 max-h-48 overflow-hidden rounded-lg border">
         <Image src={image.src} alt={name} layout="fill" objectFit="contain" />
       </div>
       <div className="grid gap-1">
         <h3 className="font-medium">{name}</h3>
         <p className="text-muted-foreground">{role}</p>
+        <p>
+          {socials?.map((item, index) => {
+            const Icon = item.icon && Icons[item.icon];
+
+            return (
+              <>
+                <Link href={item.href} target="_blank">
+                  {item.icon ? <Icon className={cn("mr-2 h-4 w-4")} /> : null}
+                </Link>
+              </>
+            );
+          })}
+        </p>
       </div>
     </div>
   );
--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -1,7 +1,5 @@
 import { allUnrelateds } from "contentlayer/generated";
 
-import { Separator } from "@openstatus/ui";
-
 import { Mdx } from "@/components/content/mdx";
 import { Shell } from "@/components/dashboard/shell";
 import { MarketingLayout } from "@/components/layout/marketing-layout";
@@ -14,29 +12,11 @@ export default function AboutPage() {
   return (
     <MarketingLayout>
       <div className="my-8 grid w-full gap-12">
-        <div className="grid w-full gap-8">
-          <h1 className="text-foreground font-cal text-4xl">
+        <div className="grid w-full gap-3">
+          <h1 className="text-foreground font-cal mb-6 text-center text-4xl">
             About OpenStatus
           </h1>
-          <p className="text-muted-foreground max-w-3xl text-lg leading-relaxed">
-            We are a small team of developers who are passionate about open
-            source and the latest tech. We are building OpenStatus to help
-            developers & companies to monitor their services from around the
-            world and inform their users when there are issues.
-          </p>
-          <ul className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:gap-6 md:grid-cols-4 md:gap-8">
-            {members.map((member) => (
-              <li key={member.name}>
-                <Member {...member} />
-              </li>
-            ))}
-          </ul>
-        </div>
-        <Separator />
-        <div className="grid w-full gap-3">
-          <p className="text-muted-foreground text-center text-lg font-medium">
-            Our Story
-          </p>
+
           <Shell className="dark:border-foreground/40 mx-auto w-auto px-6 py-6 shadow-md sm:px-8 sm:py-8 md:px-12 md:py-12">
             {story ? (
               <Mdx
@@ -47,19 +27,45 @@ export default function AboutPage() {
           </Shell>
         </div>
       </div>
+      <div className="mx-auto grid w-auto  w-full gap-8 px-6 py-6  sm:px-8 sm:py-8 md:px-12 md:py-12">
+        <h2 className="text-foreground font-cal mb-6 text-center text-2xl">
+          The Team
+        </h2>
+        <ul className="grid grid-cols-1 place-items-center gap-4  sm:grid-cols-2  sm:gap-6  md:gap-8">
+          {members.map((member) => (
+            <li key={member.name}>
+              <Member {...member} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </MarketingLayout>
   );
 }
 
 const members: MemberProps[] = [
   {
-    name: "Thibault Le Ouay",
-    role: "Developer",
-    image: { src: "/assets/authors/thibault.jpeg" },
+    name: "Maximilian Kaske",
+    role: "Pilsner Advocate",
+    image: { src: "/assets/authors/max.png" },
+    socials: [
+      {
+        label: "Twitter",
+        href: "https://twitter.com/mxkaske",
+        icon: "twitter",
+      },
+    ],
   },
   {
-    name: "Maximilian Kaske",
-    role: "Developer",
-    image: { src: "/assets/authors/max.png" },
+    name: "Thibault Le Ouay Ducasse",
+    role: "Gose Lover",
+    image: { src: "/assets/authors/thibault.jpeg" },
+    socials: [
+      {
+        label: "Twitter",
+        href: "https://twitter.com/thibaultleouay",
+        icon: "twitter",
+      },
+    ],
   },
 ];
