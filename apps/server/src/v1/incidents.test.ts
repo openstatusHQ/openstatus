80271f4a2d09c3a38c855b038cb6d9880f0b4ced
Kushagra <skushagra.sharma@gmail.com>
2024-02-03 10:22:03 +0530

    Incedents and statusReportUpdates

--- apps/server/src/v1/incidents.test.ts
+++ apps/server/src/v1/incidents.test.ts
@@ -45,6 +45,58 @@ test("Update an incident ", async () => {
   });
 });
 
+test("Update an incident-404", async () => {
+  const res = await api.request("/incident/404", {//accessing invalid monitor
+    method: "PUT",
+    headers: {
+      "x-openstatus-key": "1",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      acknowledgedAt: "2023-11-08T21:03:13.000Z",
+    }),
+  });
+  expect(res.status).toBe(200);
+  expect((await res.json())).toMatchObject({
+   code: 404,
+   message:"Not Found" 
+  });
+})
+
+test("Update an incident-401", async () => {
+  const res = await api.request("/incident/2", {
+    method: "PUT",
+    headers: {//not passing correct key
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      acknowledgedAt: "2023-11-08T21:03:13.000Z",
+    }),
+  });
+  expect(res.status).toBe(401);
+});
+
+test("Update an incident-403", async () => {
+  const res = await api.request("/incident/2", {
+    method: "PUT",
+    headers: {
+      "x-openstatus-key": "1",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({//passing incorrect body
+      acknowledgedAt: "2023-11-0",
+    }),
+  });
+  expect(res.status).toBe(400);
+  expect((await res.json())).toMatchObject({
+    error:{
+      issues: expect.any(Array),
+      name: "ZodError"
+    },
+    success: false,
+  });
+});
+
 test("Get all Incidents", async () => {
   const res = await api.request("/incident", {
     method:"GET",
