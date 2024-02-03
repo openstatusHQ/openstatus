1aaf07a50e0483268f14d809bf1dbc7f9784539d
Kushagra <skushagra.sharma@gmail.com>
2024-02-03 09:00:08 +0530

    Added error code tests for monitor

--- apps/server/src/v1/monitor.test.ts
+++ apps/server/src/v1/monitor.test.ts
@@ -1,5 +1,4 @@
 import { expect, test } from "bun:test";
-
 import { api } from ".";
 
 test("GET one monitor", async () => {
@@ -82,6 +81,61 @@ test("Create a monitor", async () => {
   });
 });
 
+//Unauthorized
+test("Create a monitor -401", async () => {
+  const data = {
+    periodicity: "10m",
+    url: "https://www.openstatus.dev",
+    name: "OpenStatus",
+    description: "OpenStatus website",
+    regions: ["ams", "gru"],
+    method: "POST",
+    body: '{"hello":"world"}',
+    headers: [{ key: "key", value: "value" }],
+    active: true,
+  };
+  const res = await api.request("/monitor", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  console.log(await res.text());
+  expect(res.status).toBe(401);
+});
+
+test("Create a monitor- 403", async () => {
+  const data = {
+    periodicity: 32, //not valid value
+    url: "https://www.openstatus.dev",
+    name: "OpenStatus",
+    description: "OpenStatus website",
+    regions: ["ams", "gru"],
+    method: "POST",
+    body: '{"hello":"world"}',
+    headers: [{ key: "key", value: "value" }],
+    active: true,
+  };
+  const res = await api.request("/monitor", {
+    method: "POST",
+    headers: {
+      "x-openstatus-key": "1",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  // console.log(await res.text());
+  expect(res.status).toBe(400);
+  expect(await res.json()).toMatchObject({
+    error:{
+      issues: expect.any(Array),
+      name: "ZodError"
+    },
+    success: false,
+  });
+});
+
 test("Update a Monitor ", async () => {
   const data = {
     periodicity: "10m",
@@ -118,6 +172,83 @@ test("Update a Monitor ", async () => {
     active: true,
   });
 });
+
+test("Update a Monitor-404 ", async () => {
+  const data = {
+    periodicity: "10m",
+    url: "https://www.openstatus.dev",
+    name: "OpenStatus",
+    description: "OpenStatus website",
+    regions: ["ams"],
+    method: "PUT",
+    body: '{"hello":"world"}',
+    headers: [{ key: "key", value: "value" }],
+    active: true,
+  };
+
+  const res = await api.request("/monitor/404", {//accessing wrong monitor, just returns 404
+    method: "PUT",
+    headers: {
+      "x-openstatus-key": "1",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  expect(res.status).toBe(200);
+
+  expect(await res.json()).toMatchObject({
+    code: 404,
+    message: "Not Found"
+  });
+});
+//not having the key return unauthorized
+test("Update a Monitor-401", async () => {
+  const data = {
+    periodicity: "5m",
+    url: "https://www.openstatus.dev",
+    name: "OpenStatus",
+    description: "OpenStatus website",
+    regions: ["ams"],
+    method: "PUT",
+    body: '{"hello":"world"}',
+    headers: [{ key: "key", value: "value" }],
+    active: true,
+  };
+  const res = await api.request("/monitor/3", {
+    method: "PUT",
+    body: JSON.stringify(data),
+  });
+  expect(res.status).toBe(401);
+})
+test("Update a Monitor-403", async () => {
+  const data = {
+    periodicity: 9,//not passing correct value returns 403
+    url: "https://www.openstatus.dev",
+    name: "OpenStatus",
+    description: "OpenStatus website",
+    regions: ["ams"],
+    method: "PUT",
+    body: '{"hello":"world"}',
+    headers: [{ key: "key", value: "value" }],
+    active: true,
+  };
+  const res = await api.request("/monitor/3", {
+    method: "PUT",
+    headers: {
+      "x-openstatus-key": "1",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  expect(res.status).toBe(400);
+  expect(await res.json()).toMatchObject({
+    error:{
+      issues: expect.any(Array),
+      name: "ZodError"
+    },
+    success: false,
+  });
+})
 
 test("Delete one monitor", async () => {
   const res = await api.request("/monitor/3", {
