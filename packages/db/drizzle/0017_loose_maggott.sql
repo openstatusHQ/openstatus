78363afcb5fb88d60995cc99ff25ccfb09142d04
mxkaske <maximilian@kaske.org>
2024-02-10 14:44:36 +0100

    Revert "fix: migration"
    This reverts commit 6b826d604624d0d294fd4c3db31bc6d38a258c40.

--- packages/db/drizzle/0017_loose_maggott.sql
+++ packages/db/drizzle/0017_loose_maggott.sql
@@ -1,14 +1,5 @@
-ALTER TABLE status_report_to_monitors ADD `created_at`;--> statement-breakpoint
-ALTER TABLE status_report_to_monitors ALTER COLUMN  `created_at` to `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-
-ALTER TABLE status_reports_to_pages ADD `created_at`;--> statement-breakpoint
-ALTER TABLE status_reports_to_pages ALTER COLUMN  `created_at` to `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-
-ALTER TABLE monitors_to_pages ADD `created_at`;--> statement-breakpoint
-ALTER TABLE monitors_to_pages ALTER COLUMN  `created_at` to `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-
-ALTER TABLE users_to_workspaces ADD `created_at`;--> statement-breakpoint
-ALTER TABLE users_to_workspaces ALTER COLUMN  `created_at` to `created_at` integer DEFAULT (strftime('%s', 'now'));--> statement-breakpoint
-
-ALTER TABLE notifications_to_monitors ADD `created_at`;--> statement-breakpoint
-ALTER TABLE notifications_to_monitors ALTER COLUMN  `created_at` to `created_at` integer DEFAULT (strftime('%s', 'now'));
+ALTER TABLE status_report_to_monitors ADD `created_at` integer DEFAULT (strftime('%s', 'now')); --> statement-breakpoint
+ALTER TABLE status_reports_to_pages ADD `created_at` integer DEFAULT (strftime('%s', 'now')); --> statement-breakpoint
+ALTER TABLE monitors_to_pages ADD `created_at` integer DEFAULT (strftime('%s', 'now')); --> statement-breakpoint
+ALTER TABLE users_to_workspaces ADD `created_at` integer DEFAULT (strftime('%s', 'now')); --> statement-breakpoint
+ALTER TABLE notifications_to_monitors ADD `created_at` integer DEFAULT (strftime('%s', 'now'));
