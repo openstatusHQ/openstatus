ef722e00aa158a2c22aef76ee53fd3aa226bcc10
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-02-28 17:22:17 +0100

    💌 more follow up email

---
+++ packages/emails/emails/followup.tsx
@@ -0,0 +1,40 @@
+import { Body, Head, Html, Link, Preview } from "@react-email/components";
+
+const FollowUpEmail = () => {
+  return (
+    <Html>
+      <Head>
+        <title>How's it going with OpenStatus?</title>
+        <Preview>How's it going with OpenStatus?</Preview>
+        <Body>
+          Hey
+          <br />
+          <br />
+          How’s everything going with Tinybird so far? Let me know if you run
+          into any issues, or have any feedback, good or bad!
+          <br />
+          <br />
+          Feel free to shoot me an email or schedule a call with me here:
+          <a href="https://cal.com/team/openstatus/30min">
+            https://cal.com/team/openstatus/30min
+          </a>
+          .
+          <br />
+          <br />
+          Thank you,
+          <br />
+          <br />
+          Thibault Le Ouay Ducasse
+          <br />
+          <br />⭐ Star us on{" "}
+          <Link href="https://github.com/openstatushq/openstatus">GitHub</Link>
+          <br />
+          🚀 Visit our website{" "}
+          <Link href="https://www.openstatus.dev">OpenStatus.dev</Link>
+        </Body>
+      </Head>
+    </Html>
+  );
+};
+
+export { FollowUpEmail };
