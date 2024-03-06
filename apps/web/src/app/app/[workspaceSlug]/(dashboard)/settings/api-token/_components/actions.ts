f9c62e29ee22fa1b23b999affd1a0ce0ebc51b69
mxkaske <maximilian@kaske.org>
2024-03-06 13:43:32 +0100

    fix: server action

--- apps/web/src/app/app/[workspaceSlug]/(dashboard)/settings/api-token/_components/actions.ts
+++ apps/web/src/app/app/[workspaceSlug]/(dashboard)/settings/api-token/_components/actions.ts
@@ -1,12 +1,32 @@
 "use server";
 
+import { currentUser } from "@clerk/nextjs";
 import { Unkey } from "@unkey/api";
+
+import { db, eq } from "@openstatus/db";
+import { user, usersToWorkspaces, workspace } from "@openstatus/db/src/schema";
 
 import { env } from "@/env";
 
 const unkey = new Unkey({ token: env.UNKEY_TOKEN, cache: "no-cache" });
 
 export async function create(ownerId: number) {
+  const _user = await currentUser();
+
+  if (!_user) return;
+
+  const allowedWorkspaces = await db
+    .select()
+    .from(usersToWorkspaces)
+    .innerJoin(user, eq(user.id, usersToWorkspaces.userId))
+    .innerJoin(workspace, eq(workspace.id, usersToWorkspaces.workspaceId))
+    .where(eq(user.tenantId, _user.id))
+    .all();
+
+  const allowedIds = allowedWorkspaces.map((i) => i.workspace.id);
+
+  if (!allowedIds.includes(ownerId)) return;
+
   const key = await unkey.keys.create({
     apiId: env.UNKEY_API_ID,
     ownerId: String(ownerId),
