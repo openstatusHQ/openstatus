e9b37a5642e0e89cb82030af0e34c3e041f472a8
mxkaske <maximilian@kaske.org>
2024-03-08 11:34:56 +0100

    refactor: remove unused interception route

--- apps/web/src/lib/tb.ts
+++ apps/web/src/lib/tb.ts
@@ -2,14 +2,12 @@ import type {
   HomeStatsParams,
   MonitorListParams,
   ResponseDetailsParams,
-  ResponseListParams,
 } from "@openstatus/tinybird";
 import {
   getHomeMonitorList,
   getHomeStats,
   getMonitorList,
   getResponseDetails,
-  getResponseList,
   Tinybird,
 } from "@openstatus/tinybird";
 
@@ -17,17 +15,6 @@ import { env } from "@/env";
 
 // @depreciated in favor to use the OSTinybird client directly
 const tb = new Tinybird({ token: env.TINY_BIRD_API_KEY });
-
-// TODO: delete related pages as it was they are just not being used
-export async function getResponseListData(props: Partial<ResponseListParams>) {
-  try {
-    const res = await getResponseList(tb)(props);
-    return res.data;
-  } catch (e) {
-    console.error(e);
-  }
-  return;
-}
 
 // TODO: not yet converted to new mv
 export async function getResponseDetailsData(
