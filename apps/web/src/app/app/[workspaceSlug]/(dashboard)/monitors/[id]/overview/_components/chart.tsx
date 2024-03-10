643792b9fec1096f3489f27745a2dc70e64f0a7d
mxkaske <maximilian@kaske.org>
2024-03-10 20:16:15 +0100

    feat: tooltip on click text opacity

--- apps/web/src/app/app/[workspaceSlug]/(dashboard)/monitors/[id]/overview/_components/chart.tsx
+++ apps/web/src/app/app/[workspaceSlug]/(dashboard)/monitors/[id]/overview/_components/chart.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import type { CustomTooltipProps } from "@tremor/react";
+import { useState } from "react";
+import type { CustomTooltipProps, EventProps } from "@tremor/react";
 import { LineChart } from "@tremor/react";
 
 import type { Region } from "@openstatus/tinybird";
 
+import { cn } from "@/lib/utils";
 import { dataFormatter, regionFormatter } from "./utils";
 
 interface ChartProps {
@@ -13,6 +15,7 @@ interface ChartProps {
 }
 
 export function Chart({ data, regions }: ChartProps) {
+  const [value, setValue] = useState<EventProps | null>(null);
   return (
     <LineChart
       data={data}
@@ -38,18 +41,22 @@ export function Chart({ data, regions }: ChartProps) {
         "pink",
         "emerald",
       ]}
-      onValueChange={(v) => void 0} // that prop makes the chart interactive
+      onValueChange={setValue}
       valueFormatter={dataFormatter}
       curveType="monotone"
       intervalType="equidistantPreserveStart"
-      customTooltip={customTooltip}
+      customTooltip={(props) => customTooltip({ ...props, value })}
       showAnimation={true}
     />
   );
 }
 
-// TBD: add custom tooltip
-const customTooltip = ({ payload, active, label }: CustomTooltipProps) => {
+const customTooltip = ({
+  payload,
+  active,
+  label,
+  value,
+}: CustomTooltipProps & { value: EventProps }) => {
   if (!active || !payload) return null;
   return (
     <div className="rounded-tremor-default text-tremor-default dark:text-dark-tremor-default bg-tremor-background dark:bg-dark-tremor-background shadow-tremor-dropdown border-tremor-border dark:border-dark-tremor-border border p-2">
@@ -59,21 +66,29 @@ const customTooltip = ({ payload, active, label }: CustomTooltipProps) => {
         </p>
         {payload
           .filter((category) => category.type !== undefined) // tremor adds additional data to the payload, we don't want that
-          .map((category, idx) => (
-            <div key={idx} className="flex flex-1 gap-2">
+          .map((category, idx) => {
+            const isActive = value
+              ? value.categoryClicked === category.dataKey
+              : true;
+            return (
               <div
-                className={`flex w-1 flex-col bg-${category.color}-500 rounded`}
-              />
-              <div className="flex w-full justify-between gap-2">
-                <p className="text-tremor-content dark:text-dark-tremor-content shrink-0">
-                  {regionFormatter(category.dataKey as Region)}
-                </p>
-                <p className="text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-mono font-medium">
-                  {dataFormatter(category.value as number)}
-                </p>
+                key={idx}
+                className={cn("flex flex-1 gap-2", !isActive && "opacity-60")}
+              >
+                <div
+                  className={`flex w-1 flex-col bg-${category.color}-500 rounded`}
+                />
+                <div className="flex w-full justify-between gap-2">
+                  <p className="text-tremor-content dark:text-dark-tremor-content shrink-0">
+                    {regionFormatter(category.dataKey as Region)}
+                  </p>
+                  <p className="text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-mono font-medium">
+                    {dataFormatter(category.value as number)}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
       </div>
     </div>
   );
