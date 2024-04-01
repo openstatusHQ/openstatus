78d79dfaa034f45df8c7c75c120d066b369bd80e
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-04-01 21:30:32 +0200

    🐛 fix smart user

--- apps/web/src/app/api/checker/test/route.ts
+++ apps/web/src/app/api/checker/test/route.ts
@@ -15,19 +15,27 @@ export function GET() {
 }
 
 export async function POST(request: Request) {
-  const json = await request.json();
-  const _valid = payloadSchema
-    .pick({ url: true, method: true, headers: true, body: true })
-    .merge(z.object({ region: monitorFlyRegionSchema.default("ams") }))
-    .safeParse(json);
+  try {
+    const json = await request.json();
+    const _valid = payloadSchema
+      .pick({ url: true, method: true, headers: true, body: true })
+      .merge(z.object({ region: monitorFlyRegionSchema.default("ams") }))
+      .safeParse(json);
 
-  if (!_valid.success) {
+    if (!_valid.success) {
+      return NextResponse.json({ success: false }, { status: 400 });
+    }
+
+    const { url, region, method, headers, body } = _valid.data;
+    // 🧑‍💻 for the smart one who want to create a loop hole
+    if (url === "https://www.openstatus.dev/api/checker/test") {
+      return NextResponse.json({ success: true }, { status: 200 });
+    }
+    const res = await checkRegion(url, region, { method, headers, body });
+
+    return NextResponse.json(res);
+  } catch (e) {
+    console.error(e);
     return NextResponse.json({ success: false }, { status: 400 });
   }
-
-  const { url, region, method, headers, body } = _valid.data;
-
-  const res = await checkRegion(url, region, { method, headers, body });
-
-  return NextResponse.json(res);
 }
