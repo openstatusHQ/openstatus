96600dec9d2734c388df8120c7627233de07fb14
mxkaske <maximilian@kaske.org>
2024-05-20 21:35:02 +0200

    chore: improve API and test coverage

--- apps/server/src/v1/page.test.ts
+++ apps/server/src/v1/page.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "bun:test";
 
 import { api } from ".";
 
-test.only("Create a page", async () => {
+test("Create a page", async () => {
   const data = {
     title: "OpenStatus",
     description: "OpenStatus website",
@@ -23,6 +23,54 @@ test.only("Create a page", async () => {
     title: "OpenStatus",
     description: "OpenStatus website",
     slug: "openstatus",
+  });
+});
+
+test("Create a page with monitors", async () => {
+  const data = {
+    title: "OpenStatus",
+    description: "OpenStatus website",
+    slug: "new-openstatus",
+    monitors: [1, 2],
+  };
+  const res = await api.request("/page", {
+    method: "POST",
+    headers: {
+      "x-openstatus-key": "1",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  expect(res.status).toBe(200);
+
+  expect(await res.json()).toMatchObject({
+    id: expect.any(Number),
+    title: "OpenStatus",
+    description: "OpenStatus website",
+    slug: "new-openstatus",
+  });
+});
+
+test("Update a page with monitors as object including order", async () => {
+  const data = {
+    monitors: [
+      { monitorId: 1, order: 0 },
+      { monitorId: 2, order: 1 },
+    ],
+  };
+  const res = await api.request("/page/3", {
+    method: "PUT",
+    headers: {
+      "x-openstatus-key": "1",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  expect(res.status).toBe(200);
+
+  expect(await res.json()).toMatchObject({
+    id: expect.any(Number),
   });
 });
 
