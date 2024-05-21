dada9a9ad2037cd26de7b3d67cb30626283308b4
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-05-21 14:50:42 +0200

    🚧 wip rum

--- packages/api/src/router/rum/index.ts
+++ packages/api/src/router/rum/index.ts
@@ -21,104 +21,13 @@ export const rumRouter = createTRPCRouter({
         event: event,
       }),
     )
-    .query(async (opts) => {
-      try {
-        const data = await opts.ctx.clickhouseClient.query({
-          query: `
-              select
-                event_name,
-                quantile(0.75)(value) as median
-              from cwv
-              where
-                dsn = '${opts.ctx.workspace.dsn}'
-                and event_name = '${opts.input.event}'
-              group by event_name
-        `,
-          format: "JSONEachRow",
-        });
-        const result = await data.json();
-        const schema = z.array(
-          z.object({ event_name: z.string(), median: z.number() }),
-        );
-        if (!result) {
-          return null;
-        }
-        const d = schema.parse(result);
-        return d.length > 0 ? d[0] : null;
-      } catch (e) {
-        return null;
-      }
+    .query((opts) => {
+      // FIXME: Use tb pipe instead
+      return null;
     }),
 
-  GetAggregatedPerPage: protectedProcedure.query(async (opts) => {
-    const data = await opts.ctx.clickhouseClient.query({
-      query: `
-      select
-        count(*) as total_event,
-        href
-      from
-          cwv
-      where
-          dsn = '${opts.ctx.workspace.dsn}'
-      group by
-          href
-      order by
-          total_event desc
-      limit
-          20
-      `,
-      format: "JSONEachRow",
-    });
-    const result = await data.json();
-    const schema = z.array(
-      z.object({ href: z.string(), total_event: z.coerce.number() }),
-    );
-    if (!result) {
-      return null;
-    }
-    const totalRoute = schema.parse(result);
-
-    const allData = [];
-    for (const currentRoute of totalRoute) {
-      const pageData = await opts.ctx.clickhouseClient.query({
-        query: `
-        select
-          quantile(0.75)(value) as value,
-          event_name
-        from
-            cwv
-        where
-            dsn = '${opts.ctx.workspace.dsn}'
-            and href = '${currentRoute.href}'
-        group by
-            event_name
-        `,
-        format: "JSONEachRow",
-      });
-      const result = await pageData.json();
-
-      if (!result) {
-        return null;
-      }
-      const schema = z.array(
-        z.object({ event_name: event, value: z.number() }),
-      );
-      const d = schema.parse(result);
-      const r = d.reduce((acc, curr) => {
-        acc = {
-          ...acc,
-          [`${String(curr.event_name).toLowerCase()}Value`]: curr.value,
-        };
-
-        return acc;
-      });
-      allData.push({
-        ...currentRoute,
-        ...r,
-      });
-    }
-    // console.log(allData);
-    // return;
-    return z.array(RouteData).parse(allData);
+  GetAggregatedPerPage: protectedProcedure.query((opts) => {
+    // FIXME: Use tb pipe instead
+    return null;
   }),
 });
