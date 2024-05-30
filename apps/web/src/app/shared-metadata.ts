3b6576958a471e31f6ab9e8a8d687f93c8cc8e72
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-05-30 11:23:29 +0200

    🌐 improve metadata

--- apps/web/src/app/shared-metadata.ts
+++ apps/web/src/app/shared-metadata.ts
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const TITLE = "OpenStatus";
 export const DESCRIPTION =
-  "A better way to monitor your services. Don't let your downtime ruin your day.";
+  "A better way to monitor your API and your frontend performance. Don't let downtime or slow page loading ruin your user experience. Speed Matters.";
 
 export const defaultMetadata: Metadata = {
   title: {
