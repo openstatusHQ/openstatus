c5f874fa637f39e0971f29309a3d9ae76aeef0cd
mxkaske <maximilian@kaske.org>
2024-06-02 16:02:31 +0200

    chore: improve middleware

--- apps/server/src/v1/middleware.ts
+++ apps/server/src/v1/middleware.ts
@@ -2,7 +2,9 @@ import { verifyKey } from "@unkey/api";
 import type { Context, Next } from "hono";
 
 import type { Variables } from "./index";
-import { getLimitByWorkspaceId } from "./utils";
+import { workspace } from "@openstatus/db/src/schema";
+import { db, eq } from "@openstatus/db";
+import { getPlanConfig } from "@openstatus/plans";
 
 export async function middleware(
   c: Context<{ Variables: Variables }, "/*">,
@@ -10,25 +12,36 @@ export async function middleware(
 ) {
   const key = c.req.header("x-openstatus-key");
   if (!key) return c.text("Unauthorized", 401);
-  if (process.env.NODE_ENV === "production") {
-    const { error, result } = await verifyKey(key);
 
-    if (error) return c.text("Internal Server Error", 500);
-    if (!result.valid) return c.text("Unauthorized", 401);
-    if (!result.ownerId) return c.text("Unauthorized", 401);
+  const { error, result } =
+    process.env.NODE_ENV === "production"
+      ? await verifyKey(key)
+      : { result: { valid: true, ownerId: "1" }, error: null };
 
-    const plan = await getLimitByWorkspaceId(Number.parseInt(result.ownerId));
+  if (error) return c.text("Internal Server Error", 500);
+  if (!result.valid) return c.text("Unauthorized", 401);
+  if (!result.ownerId) return c.text("Unauthorized", 401);
 
-    c.set("workspacePlan", plan);
-    c.set("workspaceId", `${result.ownerId}`);
-  } else {
-    // REMINDER: localhost only
-    const ownerId = 1;
+  const _workspace = await db
+    .select()
+    .from(workspace)
+    .where(eq(workspace.id, Number.parseInt(result.ownerId)))
+    .get();
 
-    const plan = await getLimitByWorkspaceId(ownerId);
-
-    c.set("workspacePlan", plan);
-    c.set("workspaceId", `${ownerId}`);
+  if (!_workspace) {
+    console.error("Workspace not found");
+    return c.text("Unauthorized", 401);
   }
+
+  c.set("workspacePlan", getPlanConfig(_workspace.plan));
+  c.set("workspaceId", `${result.ownerId}`);
+
   await next();
 }
+
+/**
+ * TODO: move the plan limit into the Unkey `{ meta }` to avoid an additional db call.
+ * When an API Key is created, we need to include the `{ meta: { plan: "free" } }` to the key.
+ * Then, we can just read the plan from the key and use it in the middleware.
+ * Don't forget to update the key whenever a user changes their plan. (via `stripeRoute` webhook)
+ */
--- packages/plans/src/utils.ts
+++ packages/plans/src/utils.ts
@@ -6,11 +6,15 @@ import type { Limits } from "./types";
 // TODO: use getLimit utils function
 export function getLimit<T extends keyof Limits>(
   plan: WorkspacePlan,
-  limit: T,
+  limit: T
 ) {
   return allPlans[plan].limits[limit];
 }
 
-export function getLimits(plan: WorkspacePlan) {
-  return allPlans[plan].limits;
+export function getLimits(plan: WorkspacePlan | null) {
+  return allPlans[plan || "free"].limits;
+}
+
+export function getPlanConfig(plan: WorkspacePlan | null) {
+  return allPlans[plan || "free"];
 }
