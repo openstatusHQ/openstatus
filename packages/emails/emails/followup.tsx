abe35ef25f2c9cb718be819805f19f334dc687e7
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-06-05 19:35:05 +0200

    📝 more content (#866)

--- packages/emails/emails/followup.tsx
+++ packages/emails/emails/followup.tsx
@@ -14,10 +14,8 @@ const FollowUpEmail = () => {
           into any issues, or have any feedback, good or bad!
           <br />
           <br />
-          Feel free to shoot me an email or schedule a call with me here:
-          <a href="https://cal.com/team/openstatus/30min">
-            https://cal.com/team/openstatus/30min
-          </a>
+          Feel free to shoot me an email or schedule a call with me{" "}
+          <a href="https://cal.com/team/openstatus/30min">here</a>
           .
           <br />
           <br />
@@ -28,7 +26,8 @@ const FollowUpEmail = () => {
           <br />
           <br />⭐ Star us on{" "}
           <Link href="https://github.com/openstatushq/openstatus">GitHub</Link>
-          <br />🚀 Visit our website{" "}
+          <br />
+          🚀 Visit our website{" "}
           <Link href="https://www.openstatus.dev">OpenStatus.dev</Link>
         </Body>
       </Head>
