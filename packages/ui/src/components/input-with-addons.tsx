a0a4d98b1b772cc38808a65c4dcd4408d4bff15a
Maximilian Kaske <maximilian@kaske.org>
2024-06-19 22:46:04 +0200

    fix: dark mdoe

--- packages/ui/src/components/input-with-addons.tsx
+++ packages/ui/src/components/input-with-addons.tsx
@@ -22,7 +22,7 @@ const InputWithAddons = React.forwardRef<
       ) : null}
       <input
         className={cn(
-          "placeholder:text-muted-foreground w-full rounded-md px-3 py-2 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "placeholder:text-muted-foreground bg-background w-full rounded-md px-3 py-2 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
