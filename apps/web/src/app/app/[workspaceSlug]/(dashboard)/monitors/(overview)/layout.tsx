f8670b754f1330058c646dc2e823add73e6e7df8
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-06-25 15:42:04 +0200

    🐛 cache (#893)

--- apps/web/src/app/app/[workspaceSlug]/(dashboard)/monitors/(overview)/layout.tsx
+++ apps/web/src/app/app/[workspaceSlug]/(dashboard)/monitors/(overview)/layout.tsx
@@ -8,6 +8,7 @@ import { api } from "@/trpc/server";
 
 export const revalidate = 0; // revalidate the data at most every hour
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const isLimitReached = await api.monitor.isMonitorLimitReached.query();
