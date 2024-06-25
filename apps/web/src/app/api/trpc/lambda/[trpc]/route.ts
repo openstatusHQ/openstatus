4814f9d5bc0be16cd2b1afe73882e23213dcd287
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-06-25 13:10:28 +0200

    🚀 build

--- apps/web/src/app/api/trpc/lambda/[trpc]/route.ts
+++ apps/web/src/app/api/trpc/lambda/[trpc]/route.ts
@@ -15,6 +15,7 @@ const handler = (req: NextRequest) =>
     endpoint: "/api/trpc/lambda",
     router: lambdaRouter,
     req: req,
+    // @ts-expect-error
     createContext: () => createTRPCContext({ req }),
     onError: ({ error }) => {
       console.log("Error in tRPC handler (lambda)");
