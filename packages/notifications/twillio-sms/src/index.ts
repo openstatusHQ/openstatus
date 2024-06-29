a5c06c27812cb67f5e06c06830e2a0818cdb9890
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-06-29 23:07:11 +0200

    🚀 degraded

--- packages/notifications/twillio-sms/src/index.ts
+++ packages/notifications/twillio-sms/src/index.ts
@@ -8,14 +8,11 @@ export const sendAlert = async ({
   notification,
   statusCode,
   message,
-  // biome-ignore lint/correctness/noUnusedVariables: <explanation>
-  incidentId,
 }: {
   monitor: Monitor;
   notification: Notification;
   statusCode?: number;
   message?: string;
-  incidentId?: string;
 }) => {
   const notificationData = SmsConfigurationSchema.parse(
     JSON.parse(notification.data)
@@ -58,7 +55,48 @@ export const sendRecovery = async ({
   statusCode,
   // biome-ignore lint/correctness/noUnusedVariables: <explanation>
   message,
+}: {
+  monitor: Monitor;
+  notification: Notification;
+  statusCode?: number;
+  message?: string;
+}) => {
+  const notificationData = SmsConfigurationSchema.parse(
+    JSON.parse(notification.data)
+  );
+  const { name } = monitor;
+
+  const body = new FormData();
+  body.set("To", notificationData.sms);
+  body.set("From", "+14807252613");
+  body.set("Body", `Your monitor ${name} / ${monitor.url} is up again`);
+
+  try {
+    fetch(
+      `https://api.twilio.com/2010-04-01/Accounts/${env.TWILLIO_ACCOUNT_ID}/Messages.json`,
+      {
+        method: "post",
+        body,
+        headers: {
+          Authorization: `Basic ${btoa(
+            `${env.TWILLIO_ACCOUNT_ID}:${env.TWILLIO_AUTH_TOKEN}`
+          )}`,
+        },
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    // Do something
+  }
+};
+
+export const sendDegraded = async ({
+  monitor,
+  notification,
   // biome-ignore lint/correctness/noUnusedVariables: <explanation>
+  statusCode,
+  // biome-ignore lint/correctness/noUnusedVariables: <explanation>
+  message,
   incidentId,
 }: {
   monitor: Monitor;
@@ -75,7 +113,7 @@ export const sendRecovery = async ({
   const body = new FormData();
   body.set("To", notificationData.sms);
   body.set("From", "+14807252613");
-  body.set("Body", `Your monitor ${name} / ${monitor.url} is up again`);
+  body.set("Body", `Your monitor ${name} / ${monitor.url} is degraded `);
 
   try {
     fetch(
