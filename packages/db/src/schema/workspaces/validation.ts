d28415edc1f2a42c305e29ee62411f4755375e40
Maximilian Kaske <maximilian@kaske.org>
2024-07-22 20:53:39 +0200

    chore: use different workspace schema for dev and prod

--- packages/db/src/schema/workspaces/validation.ts
+++ packages/db/src/schema/workspaces/validation.ts
@@ -9,7 +9,11 @@ import { workspace } from "./workspace";
 export const workspacePlanSchema = z.enum(workspacePlans);
 export const workspaceRoleSchema = z.enum(workspaceRole);
 
-export const selectWorkspaceSchema = createSelectSchema(workspace)
+/**
+ * Workspace schema with limits and plan
+ * If not available in the db, the limits will be taken from the workspace plan
+ */
+const selectWorkspaceSchemaDevelopment = createSelectSchema(workspace)
   .extend({
     limits: z.string().transform((val) => {
       const parsed = JSON.parse(val);
@@ -29,6 +33,35 @@ export const selectWorkspaceSchema = createSelectSchema(workspace)
       limits: limitsV1.parse({ ...allPlans[val.plan].limits, ...val.limits }),
     };
   });
+
+/**
+ * Workspace schema with limits and plan
+ * The limits for paid users have to be defined within the db otherwise, fallbacks to free plan limits
+ */
+const selectWorkspaceSchemaProduction = createSelectSchema(workspace).extend({
+  limits: z.string().transform((val) => {
+    const parsed = JSON.parse(val);
+    const result = limitsV1.safeParse(parsed);
+    if (result.error) {
+      // Fallback to default limits
+      return limitsV1.parse({
+        ...allPlans.free.limits,
+      });
+    }
+
+    return result.data;
+  }),
+  plan: z
+    .enum(workspacePlans)
+    .nullable()
+    .default("free")
+    .transform((val) => val ?? "free"),
+});
+
+export const selectWorkspaceSchema =
+  process.env.NODE_ENV === "development"
+    ? selectWorkspaceSchemaDevelopment
+    : selectWorkspaceSchemaProduction;
 
 export const insertWorkspaceSchema = createSelectSchema(workspace);
 
