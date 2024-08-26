da454bb9df80bbc0786a4e3d0190bee6546496f8
Maximilian Kaske <maximilian@kaske.org>
2024-08-26 22:55:52 +0200

    chore: empty state

--- apps/web/src/components/ping-response-analysis/multi-region-chart.tsx
+++ apps/web/src/components/ping-response-analysis/multi-region-chart.tsx
@@ -42,7 +42,7 @@ export function MultiRegionChart({ regions }: { regions: RegionChecker[] }) {
     .sort((a, b) => a.latency - b.latency) // FIXME: seems to be off
     .map((item) => {
       const { dns, connection, tls, ttfb, transfer } = getTimingPhases(
-        item.timing,
+        item.timing
       );
       return {
         region: item.region,
@@ -54,48 +54,59 @@ export function MultiRegionChart({ regions }: { regions: RegionChecker[] }) {
       };
     });
   return (
-    <ChartContainer config={chartConfig}>
-      <BarChart accessibilityLayer data={chartData}>
-        <CartesianGrid vertical={false} />
-        <XAxis
-          dataKey="region"
-          tickLine={false}
-          tickMargin={10}
-          axisLine={false}
-          interval={0}
-          tick={(props) => {
-            const { x, y, payload } = props;
-            return (
-              <g transform={`translate(${x},${y})`}>
-                <text
-                  x={0}
-                  y={0}
-                  dy={2}
-                  textAnchor="end"
-                  fill="#666"
-                  transform="rotate(-35)"
-                  className="font-mono"
-                >
-                  {payload.value}
-                </text>
-              </g>
-            );
-          }}
-        />
-        <ChartTooltip
-          content={
-            <ChartTooltipContent
-              labelFormatter={(label) => regionFormatter(label, "long")}
-            />
-          }
-        />
-        <ChartLegend content={<ChartLegendContent />} />
-        <Bar dataKey="dns" stackId="a" fill="var(--color-dns)" />
-        <Bar dataKey="connection" stackId="a" fill="var(--color-connection)" />
-        <Bar dataKey="tls" stackId="a" fill="var(--color-tls)" />
-        <Bar dataKey="ttfb" stackId="a" fill="var(--color-ttfb)" />
-        <Bar dataKey="transfer" stackId="a" fill="var(--color-transfer)" />
-      </BarChart>
-    </ChartContainer>
+    <div className="relative">
+      <ChartContainer config={chartConfig}>
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="region"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            interval={0}
+            tick={(props) => {
+              const { x, y, payload } = props;
+              return (
+                <g transform={`translate(${x},${y})`}>
+                  <text
+                    x={0}
+                    y={0}
+                    dy={2}
+                    textAnchor="end"
+                    fill="#666"
+                    transform="rotate(-35)"
+                    className="font-mono"
+                  >
+                    {payload.value}
+                  </text>
+                </g>
+              );
+            }}
+          />
+          <ChartTooltip
+            content={
+              <ChartTooltipContent
+                labelFormatter={(label) => regionFormatter(label, "long")}
+              />
+            }
+          />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey="dns" stackId="a" fill="var(--color-dns)" />
+          <Bar
+            dataKey="connection"
+            stackId="a"
+            fill="var(--color-connection)"
+          />
+          <Bar dataKey="tls" stackId="a" fill="var(--color-tls)" />
+          <Bar dataKey="ttfb" stackId="a" fill="var(--color-ttfb)" />
+          <Bar dataKey="transfer" stackId="a" fill="var(--color-transfer)" />
+        </BarChart>
+      </ChartContainer>
+      {regions.length ? null : (
+        <div className="absolute inset-0 flex w-full items-center justify-center bg-muted/50">
+          <div className="text-foreground">No results</div>
+        </div>
+      )}
+    </div>
   );
 }
