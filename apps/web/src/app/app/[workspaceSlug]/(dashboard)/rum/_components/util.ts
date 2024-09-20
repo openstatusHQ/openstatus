6a9cf14d85e67a94dc5603da059d71f27bb5205f
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-09-20 22:13:37 +0200

    🗑️ some clean up

--- apps/web/src/app/app/[workspaceSlug]/(dashboard)/rum/_components/util.ts
+++
@@ -1,6 +0,0 @@
-export const timeFormater = (time: number) => {
-  if (time < 1000) {
-    return `${new Intl.NumberFormat("us").format(time).toString()}ms`;
-  }
-  return `${new Intl.NumberFormat("us").format(time / 1000).toString()}s`;
-};
