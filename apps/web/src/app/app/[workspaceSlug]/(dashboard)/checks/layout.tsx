41af9772112b67224e207009f273bcc7ccae2688
Maximilian Kaske <maximilian@kaske.org>
2024-10-04 09:20:54 +0200

    chore: add action button for api docs

--- apps/web/src/app/app/[workspaceSlug]/(dashboard)/checks/layout.tsx
+++ apps/web/src/app/app/[workspaceSlug]/(dashboard)/checks/layout.tsx
@@ -2,13 +2,27 @@ import type { ReactNode } from "react";
 
 import { Header } from "@/components/dashboard/header";
 import AppPageLayout from "@/components/layout/app-page-layout";
+import { Button } from "@openstatus/ui";
+import { ArrowUpRight } from "lucide-react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
     <AppPageLayout>
       <Header
         title="Single Checks"
-        description="Access your CI/API checks within the table."
+        description="Built custom CI/CD pipelines with OpenStatus API. Access your checks within the table."
+        actions={
+          <Button asChild>
+            <a
+              href="https://docs.openstatus.dev/api-reference/check/post-check"
+              target="_blank"
+              rel="noreferrer"
+              className="whitespace-nowrap"
+            >
+              API Docs <ArrowUpRight className="ml-1 h-4 w-4" />
+            </a>
+          </Button>
+        }
       />
       {children}
     </AppPageLayout>
