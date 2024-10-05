c441b67b85367b39c41c4ab0d3e31fa4f1d8c8de
Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>
2024-10-05 10:11:19 +0200

    chore: add banner (#1034)

--- apps/web/src/app/app/[workspaceSlug]/(dashboard)/incidents/(overview)/page.tsx
+++ apps/web/src/app/app/[workspaceSlug]/(dashboard)/incidents/(overview)/page.tsx
@@ -4,23 +4,33 @@ import { EmptyState } from "@/components/dashboard/empty-state";
 import { columns } from "@/components/data-table/incident/columns";
 import { DataTable } from "@/components/data-table/incident/data-table";
 import { api } from "@/trpc/server";
+import { ReportInfoBanner } from "./report-info-banner";
 
 export default async function IncidentPage() {
-  const incidents = await api.incident.getIncidentsByWorkspace.query();
+  const [incidents, statusReports] = await Promise.all([
+    api.incident.getIncidentsByWorkspace.query(),
+    api.statusReport.getStatusReportByWorkspace.query(),
+  ]);
+
+  console.log(statusReports);
 
   if (incidents?.length === 0)
     return (
-      <EmptyState
-        icon="siren"
-        title="No incidents"
-        description="Hopefully you will see this screen for a long time."
-        action={undefined}
-      />
+      <>
+        <EmptyState
+          icon="siren"
+          title="No incidents"
+          description="Hopefully you will see this screen for a long time."
+          action={undefined}
+        />
+        {!statusReports.length ? <ReportInfoBanner /> : null}
+      </>
     );
 
   return (
     <>
       <DataTable columns={columns} data={incidents} />
+      {!statusReports.length ? <ReportInfoBanner /> : null}
     </>
   );
 }
