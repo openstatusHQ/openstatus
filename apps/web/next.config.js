6de70fa15e7a39bc24b939b8d7fe8de9d29af6d6
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-10-22 11:28:51 +0200

    🤔 will it work

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
   transpilePackages: ["@openstatus/ui", "@openstatus/api"],
   experimental: {
     outputFileTracingIncludes: {
-      "/": ["./node_modules/@google-cloud/tasks/build/esm/src/**/*.json"],
+      "/": ["./node_modules/.pnpm/@google-cloud/tasks/build/esm/src/**/*.json", "./node_modules/@google-cloud/tasks/build/esm/src/**/*.js"],
+
     },
     serverComponentsExternalPackages: [
       // "libsql",
@@ -51,37 +52,36 @@ const nextConfig = {
   },
 };
 
-module.exports = withContentlayer(nextConfig);
 // Injected content via Sentry wizard below
 
-// const { withSentryConfig } = require("@sentry/nextjs");
+const { withSentryConfig } = require("@sentry/nextjs");
 
-// module.exports = withSentryConfig(
-//   withContentlayer(nextConfig),
-//   {
-//     // For all available options, see:
-//     // https://github.com/getsentry/sentry-webpack-plugin#options
+module.exports = withSentryConfig(
+  withContentlayer(nextConfig),
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
 
-//     // Suppresses source map uploading logs during build
-//     silent: true,
+    // Suppresses source map uploading logs during build
+    silent: true,
 
-//     org: "openstatus",
-//     project: "openstatus",
-//   },
-//   {
-//     // For all available options, see:
-//     // // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    org: "openstatus",
+    project: "openstatus",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-//     // // Upload a larger set of source maps for prettier stack traces (increases build time)
-//     // widenClientFileUpload: true,
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-//     // // Transpiles SDK to be compatible with IE11 (increases bundle size)
-//     // transpileClientSDK: false,
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
 
-//     // // Hides source maps from generated client bundles
-//     // hideSourceMaps: true,
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
 
-//     // // Automatically tree-shake Sentry logger statements to reduce bundle size
-//     // disableLogger: true,
-//   },
-// );
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  },
+);
