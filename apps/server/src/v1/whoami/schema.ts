f2840732f3f2879ccb1695a7a42513aeaf274cb3
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-10-25 19:58:12 +0200

    ✏️ typo

--- apps/server/src/v1/whoami/schema.ts
+++ apps/server/src/v1/whoami/schema.ts
@@ -1,12 +1,8 @@
 import { z } from "@hono/zod-openapi";
-import { allPlans } from "@openstatus/db/src/schema/plan/config";
-import { limitSchema } from "@openstatus/db/src/schema/plan/schema";
+
 import { workspacePlans } from "@openstatus/db/src/schema/workspaces/constants";
 
 export const schema = z.object({
-  workspaceId: z.number().openapi({
-    description: "The current workspace id",
-  }),
   name: z
     .string()
     .openapi({ description: "The current workspace name" })
