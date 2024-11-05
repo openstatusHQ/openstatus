9ec099ae4bbd27e8400dad47ae9571f42d802bdc
Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>
2024-11-05 16:05:48 +0100

    fix: image next config (#1084)
    * fix: image next config
    
    ci: apply automated fixes
    
    * ci: apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,5 @@
 const { withContentCollections } = require("@content-collections/next");
+const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -54,12 +55,8 @@ const nextConfig = {
   },
 };
 
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
 module.exports = withSentryConfig(
-  withContentCollections(nextConfig),
+  async () => await withContentCollections(nextConfig),
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
