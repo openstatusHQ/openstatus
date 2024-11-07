a2416d4f7387544cb8a93b13aacfbf9e22f56206
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-11-07 20:31:29 +0100

    🚧 astro

---
+++ apps/docs/astro.config.mjs
@@ -0,0 +1,28 @@
+import starlight from '@astrojs/starlight';
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', slug: 'guides/example' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
+		}),
+	],
+});
