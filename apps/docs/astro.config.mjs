3f7d0110394e920e436413a4c15b80cc5d9c4da9
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-11-10 14:17:06 +0100

    🚧 docs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,28 +1,133 @@
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import starlightSidebarTopics from 'starlight-sidebar-topics'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: "OpenStatus",
+			favicon: '/favicon.ico',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/openstatusHQ/openstatus",
+				blueSky: "https://bsky.app/profile/openstatus.dev",
 			},
-			// sidebar: [
-			// 	{
-			// 		label: 'Guides',
-			// 		items: [
-			// 			// Each item here is one entry in the navigation menu.
-			// 			{ label: 'Example Guide', slug: 'guides/example' },
-			// 		],
-			// 	},
-			// 	{
-			// 		label: 'Reference',
-			// 		autogenerate: { directory: 'reference' },
-			// 	},
-			// ],
+			components: {
+				SiteTitle: "./src/components/SiteTitle.astro",
+			},
+			editLink: {
+				baseUrl: "https://github.com/openstatusHQ/openstatus/app/docs",
+			},
+			customCss: [
+				// Path to your Tailwind base styles:
+				"./src/tailwind.css",
+			],
+			plugins: [
+				starlightSidebarTopics([
+					{
+						label: "Documentation",
+						link: '/getting-started/introduction',
+						id: "docs",
+						icon: 'open-book',
+						items:
+							[
+								{
+									label: "Getting Started",
+									items: [
+										{ label: "Introduction", slug: "getting-started/introduction" },
+
+									],
+								},
+								{
+									label: "Synthetic Monitoring",
+									items: [
+										{ label: "Overview", slug: "monitoring/intro" },
+										{ label: "Create you first monitor", slug: "monitoring/create-monitor" },
+										{
+											label: "Customizations", items: [
+												{
+													label: "Assertions", slug: "monitoring/customization/assertions"
+												},
+												{
+													label: "Frequency", slug: "monitoring/customization/frequency"
+												},
+												{
+													label: "Headers", slug: "monitoring/customization/headers"
+												},
+												{
+													label: "Regions", slug: "monitoring/customization/regions"
+												},
+												{
+													label: "Timing", slug: "monitoring/customization/timing"
+												},
+
+											]
+										},
+										{
+											label: "Monitor Types", items: [
+												{
+													label: "HTTP Monitor", slug: "monitoring/type/http"
+												},
+												{
+													label: "TCP Monitor", slug: "monitoring/type/tcp"
+												}
+											]
+										},
+									],
+									collapsed: false,
+								},
+								{
+									label: "Status Page",
+									items: [],
+								},
+								{ label: "Incidents", items: [] },
+								{
+									label: "Alerting", items: [
+										{ label: "Overview", slug: "alerting/overview" },
+										{ label: "Notification Channels", autogenerate: { directory: 'alerting/providers' } },
+									]
+								},
+								{
+									label: "Support", items: [{
+										label: "Help", slug: "help/support"
+									}]
+								},
+
+
+							],
+					},
+					{
+						label: "Guides",
+						icon: 'rocket',
+						link: '/guides/introduction',
+						items: [{
+							label: "Guides",
+							autogenerate: { directory: 'guides' }
+						}]
+					},
+					{
+						label: "CLI",
+						icon: 'seti:shell',
+						link: '/guides/introduction',
+						items: [{
+							label: "Guides",
+							autogenerate: { directory: 'guides' }
+						}]
+					},
+					{
+						label: "API Reference",
+						icon: 'puzzle',
+						link: 'https://api.openstatus.dev/v1',
+
+					}
+				])
+			],
+		}),
+		tailwind({
+			// Disable the default base styles:
+			applyBaseStyles: false,
 		}),
 	],
 });
--- apps/docs/src/content/config.ts
+++ apps/docs/src/content/config.ts
@@ -1,6 +1,7 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection } from "astro:content";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { topicSchema } from "starlight-sidebar-topics/schema";
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({ schema: docsSchema({ extend: topicSchema }) }),
 };
