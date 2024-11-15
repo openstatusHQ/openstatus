642071ea6c7132ce3a390ff73f103a13d3f9e043
Maximilian Kaske <maximilian@kaske.org>
2024-11-15 21:20:26 +0100

    chore: add curl cta and question

--- apps/web/src/app/play/curl/page.tsx
+++ apps/web/src/app/play/curl/page.tsx
@@ -12,6 +12,8 @@ import {
 } from "@/components/marketing/card";
 import type { Metadata } from "next";
 import { CurlForm } from "./_components/curl-form";
+import { BottomCTA } from "@/components/marketing/in-between-cta";
+import { Shell } from "@/components/dashboard/shell";
 
 const title = "cURL Builder";
 const description =
@@ -37,28 +39,54 @@ export const metadata: Metadata = {
 
 export default function CurlBuilder() {
   return (
-    <CardContainer>
-      <CardHeader>
-        <CardIcon icon="terminal" />
-        <CardTitle>cURL Builder</CardTitle>
-        <CardDescription className="max-w-md">
-          An online curl command line builder. Generate curl commands to test
-          your API endpoints.
-        </CardDescription>
-      </CardHeader>
-      <div className="mx-auto grid w-full max-w-xl gap-6">
-        <CurlForm
-          defaultValues={{
-            method: "GET",
-            url: "",
-            body: "",
-            verbose: false,
-            insecure: false,
-            json: false,
-            headers: [],
-          }}
-        />
+    <div className="grid h-full w-full gap-12">
+      <CardContainer>
+        <CardHeader>
+          <CardIcon icon="terminal" />
+          <CardTitle>cURL Builder</CardTitle>
+          <CardDescription className="max-w-md">
+            An online curl command line builder. Generate curl commands to test
+            your API endpoints.
+          </CardDescription>
+        </CardHeader>
+        <div className="mx-auto grid w-full max-w-xl gap-8">
+          <CurlForm
+            defaultValues={{
+              method: "GET",
+              url: "",
+              body: "",
+              verbose: false,
+              insecure: false,
+              json: false,
+              headers: [],
+            }}
+          />
+        </div>
+      </CardContainer>
+      <Informations />
+      <BottomCTA />
+    </div>
+  );
+}
+
+function Informations() {
+  return (
+    <Shell>
+      <div className="grid gap-1">
+        <h3 className="font-semibold">What is cURL?</h3>
+        <p className="text-muted-foreground">
+          cURL (Client URL) is a command-line tool and library for transferring
+          data with URLs. It supports various protocols like HTTP, HTTPS, FTP,
+          and more, making it a versatile choice for testing APIs, downloading
+          files, or performing network tasks. Its simplicity and power come from
+          the ability to execute complex operations through straightforward
+          commands.
+        </p>
+        <p className="text-muted-foreground">
+          cURL is available on most operating systems, including Linux, macOS,
+          and Windows.
+        </p>
       </div>
-    </CardContainer>
+    </Shell>
   );
 }
