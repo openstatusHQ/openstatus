ae337f4b77fa173289f711defe51b6584f81178a
Maximilian Kaske <maximilian@kaske.org>
2024-11-26 10:58:35 +0100

    chore: update key replace

--- apps/workflows/src/cron/checker.ts
+++ apps/workflows/src/cron/checker.ts
@@ -25,7 +25,7 @@ const client = new CloudTasksClient({
   projectId: env().GCP_PROJECT_ID,
   credentials: {
     client_email: env().GCP_CLIENT_EMAIL,
-    private_key: env().GCP_PRIVATE_KEY,
+    private_key: env().GCP_PRIVATE_KEY.replaceAll("\\n", "\n"),
   },
 });
 
