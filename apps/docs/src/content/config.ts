2eca47ce89a6910f618eff50023b60d494b162b4
Thibault Le Ouay <thibaultleouay@gmail.com>
2024-12-05 08:53:47 +0100

    📝 improvments

--- apps/docs/src/content/config.ts
+++ apps/docs/src/content/config.ts
@@ -1,11 +1,11 @@
 import { defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
-import { glob } from "astro/loaders";
+// import { glob } from "astro/loaders";
 import { topicSchema } from "starlight-sidebar-topics/schema";
 
 export const collections = {
   docs: defineCollection({
-    loader: glob({ pattern: "**/*.mdx", base: "./src/content/docs" }),
+    // loader: glob({ pattern: "**/*.mdx", base: "./src/content/docs" }),
     schema: docsSchema({ extend: topicSchema }),
   }),
 };
