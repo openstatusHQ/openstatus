3232083f082b358426432e06d91c7210f22e7485
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2025-05-04 21:40:50 +0200

    add openpanel to docs (astro) and add web analytics to nextjs

--- apps/web/src/app/app/layout.tsx
+++ apps/web/src/app/app/layout.tsx
@@ -1,16 +1,22 @@
 import { SessionProvider } from "next-auth/react";
 
 import { Bubble } from "@/components/support/bubble";
+import { auth } from "@/lib/auth";
+import { IdentifyComponent } from "@openpanel/nextjs";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       {children}
       <Bubble />
+      {session?.user?.id && (
+        <IdentifyComponent profileId={`usr_${session?.user?.id}`} />
+      )}
     </SessionProvider>
   );
 }
