011a27deab325692e2fd8bf549d963a41b7b585a
Maximilian Kaske <maximilian@kaske.org>
2025-05-13 21:53:39 +0200

    feat: tooltip

--- apps/web/src/components/layout/header/copy-button.tsx
+++ apps/web/src/components/layout/header/copy-button.tsx
@@ -1,23 +1,36 @@
 "use client";
 
 import { useCopyToClipboard } from "@/hooks/use-copy-to-clipboard";
-import { Button } from "@openstatus/ui";
+import {
+  Button,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@openstatus/ui";
 import { Copy } from "lucide-react";
 
-export function CopyButton({ id }: { id: string }) {
+export function CopyButton({ id }: { id: string | number }) {
   const { copy } = useCopyToClipboard();
   return (
-    <Button
-      variant="ghost"
-      className="font-mono"
-      onClick={() => {
-        copy(id, {
-          withToast: `Copied ID '${id}'`,
-        });
-      }}
-    >
-      <Copy className="h-4 w-4 mr-2 text-muted-foreground" />
-      {id}
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            className="font-mono"
+            onClick={() => {
+              copy(`${id}`, {
+                withToast: `Copied ID '${id}'`,
+              });
+            }}
+          >
+            <Copy className="h-4 w-4 mr-2 text-muted-foreground" />
+            {id}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Copy ID for API usage</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
