343dc58c172e108cdc636ccd9a7dbe8c25964719
Maximilian Kaske <maximilian@kaske.org>
2025-06-13 20:52:01 +0200

    chore: middleware

--- apps/dashboard/src/middleware.ts
+++ apps/dashboard/src/middleware.ts
@@ -2,11 +2,15 @@ import { NextResponse } from "next/server";
 
 import { auth } from "@/lib/auth";
 
+import { db, eq } from "@openstatus/db";
+import { user, usersToWorkspaces, workspace } from "@openstatus/db/src/schema";
+
 export default auth(async (req) => {
   const url = req.nextUrl.clone();
+  const response = NextResponse.next();
 
   if (url.pathname.includes("api/trpc")) {
-    return NextResponse.next();
+    return response;
   }
 
   const encodedSearchParams = `${url.pathname}?${url.search}`;
@@ -21,7 +25,29 @@ export default auth(async (req) => {
     return NextResponse.redirect(newURL);
   }
 
-  return NextResponse.next();
+  const hasWorkspaceSlug = req.cookies.has("workspace-slug");
+
+  if (req.auth?.user?.id && !hasWorkspaceSlug) {
+    const [query] = await db
+      .select()
+      .from(usersToWorkspaces)
+      .innerJoin(user, eq(user.id, usersToWorkspaces.userId))
+      .innerJoin(workspace, eq(workspace.id, usersToWorkspaces.workspaceId))
+      .where(eq(user.id, Number.parseInt(req.auth.user.id)))
+      .all();
+
+    if (!query) {
+      console.error(">> Should not happen, no workspace found for user");
+    }
+
+    response.cookies.set("workspace-slug", query.workspace.slug);
+  }
+
+  if (!req.auth && hasWorkspaceSlug) {
+    response.cookies.delete("workspace-slug");
+  }
+
+  return response;
 });
 
 export const config = {
