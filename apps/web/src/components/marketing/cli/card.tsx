0c0a8418dcf77321b7315127cb2a51de6f91c81b
Maximilian Kaske <maximilian@kaske.org>
2025-06-17 21:28:00 +0200

    feat: cli card

---
+++ apps/web/src/components/marketing/cli/card.tsx
@@ -0,0 +1,32 @@
+import { cardConfig } from "@/config/features";
+import {
+  CardContainer,
+  CardContent,
+  CardFeature,
+  CardFeatureContainer,
+  CardHeader,
+  CardIcon,
+  CardTitle,
+} from "../card";
+import { Terminal } from "./terminal";
+
+export function CLICard() {
+  const { icon, title, features } = cardConfig.cli;
+  return (
+    <CardContainer>
+      <CardHeader>
+        <CardIcon icon={icon} />
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Terminal />
+        <CardFeatureContainer>
+          {features?.map((feature, i) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            <CardFeature key={i} {...feature} />
+          ))}
+        </CardFeatureContainer>
+      </CardContent>
+    </CardContainer>
+  );
+}
