12d429953744bfb5fbc5bfe45e2af21ba3591ba4
Maximilian Kaske <maximilian@kaske.org>
2025-07-06 21:29:19 +0200

    chore: tooltip

--- apps/dashboard/src/components/development-indicator.tsx
+++ apps/dashboard/src/components/development-indicator.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import * as Portal from "@radix-ui/react-portal";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 export function DevelopmentIndicator() {
   if (process.env.NODE_ENV !== "production") return null;
@@ -8,11 +14,20 @@ export function DevelopmentIndicator() {
   return (
     <Portal.Root>
       <div className="pointer-events-none fixed inset-0 z-[9999] border-2 border-destructive" />
-      <div className="pointer-events-none fixed inset-x-0 bottom-0 z-[9999] select-none">
+      <div className="fixed inset-x-0 bottom-0 z-[9999] select-none">
         <div className="flex items-center justify-center">
-          <div className="bg-destructive text-background w-fit font-mono text-xs rounded-t px-2 py-1">
-            In Development
-          </div>
+          <TooltipProvider delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="bg-destructive text-background w-fit font-mono text-xs rounded-t px-2 py-1">
+                  In Development
+                </div>
+              </TooltipTrigger>
+              <TooltipContent side="top">
+                <p>Be careful with your actions.</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
     </Portal.Root>
