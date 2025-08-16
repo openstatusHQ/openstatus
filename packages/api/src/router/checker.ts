2cae649c6e7c0cd845f7f617f79d0d006b406ea3
Thibault Le Ouay <thibaultleouay@gmail.com>
2025-08-16 13:01:09 +0200

    🚀 trigger checker after creation

--- packages/api/src/router/checker.ts
+++ packages/api/src/router/checker.ts
@@ -12,6 +12,12 @@ import { monitorFlyRegionSchema } from "@openstatus/db/src/schema/constants";
 import { z } from "zod";
 import { env } from "../env";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { selectMonitorSchema } from "@openstatus/db/src/schema";
+import {
+  type httpPayloadSchema,
+  type tpcPayloadSchema,
+  transformHeaders,
+} from "@openstatus/utils";
 
 const ABORT_TIMEOUT = 10000;
 
@@ -247,6 +253,93 @@ export async function testTcp(input: z.infer<typeof tcpTestInput>) {
   }
 }
 
+export async function triggerChecker(input: z.infer<typeof selectMonitorSchema>) {
+  let payload:
+    | z.infer<typeof httpPayloadSchema>
+    | z.infer<typeof tpcPayloadSchema>
+    | null = null;
+
+  const timestamp = Date.now();
+
+  if (input.jobType === "http") {
+    payload = {
+      workspaceId: String(input.workspaceId),
+      monitorId: String(input.id),
+      url: input.url,
+      method: input.method || "GET",
+      cronTimestamp: timestamp,
+      body: input.body,
+      headers: input.headers,
+      status: "active",
+      assertions: input.assertions ? JSON.parse(input.assertions) : null,
+      degradedAfter: input.degradedAfter,
+      timeout: input.timeout,
+      trigger: "cron",
+      otelConfig: input.otelEndpoint
+        ? {
+            endpoint: input.otelEndpoint,
+            headers: transformHeaders(input.otelHeaders),
+          }
+        : undefined,
+      retry: input.retry || 3,
+    };
+  }
+  if (input.jobType === "tcp") {
+    payload = {
+      workspaceId: String(input.workspaceId),
+      monitorId: String(input.id),
+      uri: input.url,
+      status: "active",
+      assertions: input.assertions ? JSON.parse(input.assertions) : null,
+      cronTimestamp: timestamp,
+      degradedAfter: input.degradedAfter,
+      timeout: input.timeout,
+      trigger: "cron",
+      retry: input.retry || 3,
+      otelConfig: input.otelEndpoint
+        ? {
+            endpoint: input.otelEndpoint,
+            headers: transformHeaders(input.otelHeaders),
+          }
+        : undefined,
+    };
+  }
+  const allResult = [];
+
+  for (const region of input.regions) {
+
+    const res =  fetch(
+      generateUrl({ row: input }),
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${env.CRON_SECRET}`,
+          "Content-Type": "application/json",
+          "fly-prefer-region": region,
+        },
+        body: JSON.stringify(payload),
+        signal: AbortSignal.timeout(ABORT_TIMEOUT),
+      },
+    );
+    allResult.push(res)
+  }
+
+  await Promise.allSettled(allResult);
+
+}
+
+
+function generateUrl({ row }: { row: z.infer<typeof selectMonitorSchema> }) {
+  switch (row.jobType) {
+    case "http":
+      return `https://openstatus-checker.fly.dev/checker/http?monitor_id=${row.id}`;
+    case "tcp":
+      return `https://openstatus-checker.fly.dev/checker/tcp?monitor_id=${row.id}`;
+    default:
+      throw new Error("Invalid jobType");
+  }
+}
+
 export const checkerRouter = createTRPCRouter({
   testHttp: protectedProcedure
     .meta({ track: Events.TestMonitor })
@@ -261,4 +354,8 @@ export const checkerRouter = createTRPCRouter({
     .mutation(async ({ input }) => {
       return testTcp(input);
     }),
+
+  triggerChecker: protectedProcedure.input(selectMonitorSchema).mutation(async ({ input }) => {
+    return triggerChecker(input)
+  }),
 });
