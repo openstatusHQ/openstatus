2cf1170ab9bdfaf556d1ab2df33bcaa01403f3b3
Thibault Le Ouay <thibaultleouay@gmail.Com>
2025-08-19 10:22:05 +0200

    🚑 (#1322)
    * 🚑
    
    * ci: apply automated fixes
    
    * 🚑
    
    * 😭
    
    * 🚑
    
    * 🤦
    
    * 🔥
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/workflows/src/index.ts
+++ apps/workflows/src/index.ts
@@ -8,7 +8,12 @@ import { env } from "./env";
 
 const { NODE_ENV, PORT } = env();
 
-const app = new Hono({ strict: false });
+const app = new Hono({ strict: false }) // Add an onError hook to report unhandled exceptions to Sentry.
+  .onError((err, c) => {
+    // Report _all_ unhandled errors.
+    c.get("sentry").captureException(err);
+    return c.text("Internal Server Error", 500);
+  });
 
 app.use("*", sentry({ dsn: env().SENTRY_DSN }));
 
@@ -26,11 +31,11 @@ app.get("/ping", (c) => c.json({ ping: "pong" }, 200));
  */
 app.route("/cron", cronRouter);
 
+app.route("/", checkerRoute);
+
 if (NODE_ENV === "development") {
   showRoutes(app, { verbose: true, colorize: true });
 }
-
-app.route("/", checkerRoute);
 
 console.log(`Starting server on port ${PORT}`);
 
