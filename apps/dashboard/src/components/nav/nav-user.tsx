b34387edc003ba67969bca4f4ef5de5984fd4af6
Thibault Le Ouay <thibaultleouay@gmail.Com>
2025-09-09 08:33:37 +0200

    ✏️ font drop down (#1348)
    * ✏️
    
    * ci: apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/dashboard/src/components/nav/nav-user.tsx
+++ apps/dashboard/src/components/nav/nav-user.tsx
@@ -70,7 +70,9 @@ export function NavUser() {
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{userName}</span>
-                <span className="truncate text-xs">{user?.email}</span>
+                <span className="truncate font-commit-mono text-xs">
+                  {user?.email}
+                </span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -105,6 +107,7 @@ export function NavUser() {
                   <Link
                     href="/settings/billing"
                     onClick={() => setOpenMobile(false)}
+                    className="font-commit-mono"
                   >
                     <Sparkles />
                     Upgrade Workspace
@@ -113,7 +116,7 @@ export function NavUser() {
                 <DropdownMenuSeparator />
               </>
             ) : null}
-            <DropdownMenuGroup>
+            <DropdownMenuGroup className="font-commit-mono">
               <DropdownMenuItem asChild>
                 <Link
                   href="/settings/account"
@@ -135,7 +138,7 @@ export function NavUser() {
                   Theme
                 </DropdownMenuSubTrigger>
                 <DropdownMenuPortal>
-                  <DropdownMenuSubContent>
+                  <DropdownMenuSubContent className="font-commit-mono">
                     <DropdownMenuItem onClick={() => setTheme("light")}>
                       <Sun /> Light
                     </DropdownMenuItem>
@@ -159,7 +162,10 @@ export function NavUser() {
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => signOut()}>
+            <DropdownMenuItem
+              onClick={() => signOut()}
+              className="font-commit-mono"
+            >
               <LogOut />
               Log out
             </DropdownMenuItem>
