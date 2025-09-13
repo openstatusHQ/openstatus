cb70c2f96b04e6a97b0289c2e56bf8595228f378
Maximilian Kaske <maximilian@kaske.org>
2025-09-13 10:35:20 +0200

    fix: monitors empty state

--- apps/status-page/src/app/(status-page)/[domain]/(public)/monitors/page.tsx
+++ apps/status-page/src/app/(status-page)/[domain]/(public)/monitors/page.tsx
@@ -4,16 +4,14 @@ import {
   ChartAreaPercentiles,
   ChartAreaPercentilesSkeleton,
 } from "@/components/chart/chart-area-percentiles";
-import {
-  EmptyStateContainer,
-  EmptyStateDescription,
-  EmptyStateTitle,
-} from "@/components/content/empty-state";
 import { useStatusPage } from "@/components/status-page/floating-button";
 import {
   Status,
   StatusContent,
   StatusDescription,
+  StatusEmptyState,
+  StatusEmptyStateDescription,
+  StatusEmptyStateTitle,
   StatusHeader,
   StatusTitle,
 } from "@/components/status-page/status";
@@ -37,6 +35,8 @@ export default function Page() {
 
   if (!page) return null;
 
+  const publicMonitors = page.monitors.filter((monitor) => monitor.public);
+
   return (
     <Status variant={variant}>
       <StatusHeader>
@@ -44,62 +44,60 @@ export default function Page() {
         <StatusDescription>{page.description}</StatusDescription>
       </StatusHeader>
       <StatusContent className="flex flex-col gap-6">
-        {page.monitors.length > 0 ? (
-          page.monitors
-            .filter((monitor) => monitor.public)
-            .map((monitor) => {
-              const data =
-                monitors
-                  ?.find((item) => item.id === monitor.id)
-                  ?.data?.map((item) => ({
-                    ...item,
-                    // TODO: create formatter
-                    timestamp: new Date(item.timestamp).toLocaleString(
-                      "default",
-                      {
-                        day: "numeric",
-                        month: "short",
-                        hour: "numeric",
-                        minute: "numeric",
-                        timeZoneName: "short",
-                      },
-                    ),
-                  })) ?? [];
+        {publicMonitors.length > 0 ? (
+          publicMonitors.map((monitor) => {
+            const data =
+              monitors
+                ?.find((item) => item.id === monitor.id)
+                ?.data?.map((item) => ({
+                  ...item,
+                  // TODO: create formatter
+                  timestamp: new Date(item.timestamp).toLocaleString(
+                    "default",
+                    {
+                      day: "numeric",
+                      month: "short",
+                      hour: "numeric",
+                      minute: "numeric",
+                      timeZoneName: "short",
+                    },
+                  ),
+                })) ?? [];
 
-              return (
-                <Link
-                  key={monitor.id}
-                  href={`./monitors/${monitor.id}`}
-                  className="rounded-lg"
-                >
-                  <div className="group -mx-3 -my-2 flex flex-col gap-2 rounded-lg border border-transparent px-3 py-2 hover:border-border/50 hover:bg-muted/50">
-                    <div className="flex flex-row items-center gap-2">
-                      <StatusMonitorTitle>{monitor.name}</StatusMonitorTitle>
-                      <StatusMonitorDescription>
-                        {monitor.description}
-                      </StatusMonitorDescription>
-                    </div>
-                    {isLoading ? (
-                      <ChartAreaPercentilesSkeleton className="h-[80px]" />
-                    ) : (
-                      <ChartAreaPercentiles
-                        className="h-[80px]"
-                        legendClassName="pb-1"
-                        data={data}
-                        singleSeries
-                      />
-                    )}
+            return (
+              <Link
+                key={monitor.id}
+                href={`./monitors/${monitor.id}`}
+                className="rounded-lg"
+              >
+                <div className="group -mx-3 -my-2 flex flex-col gap-2 rounded-lg border border-transparent px-3 py-2 hover:border-border/50 hover:bg-muted/50">
+                  <div className="flex flex-row items-center gap-2">
+                    <StatusMonitorTitle>{monitor.name}</StatusMonitorTitle>
+                    <StatusMonitorDescription>
+                      {monitor.description}
+                    </StatusMonitorDescription>
                   </div>
-                </Link>
-              );
-            })
+                  {isLoading ? (
+                    <ChartAreaPercentilesSkeleton className="h-[80px]" />
+                  ) : (
+                    <ChartAreaPercentiles
+                      className="h-[80px]"
+                      legendClassName="pb-1"
+                      data={data}
+                      singleSeries
+                    />
+                  )}
+                </div>
+              </Link>
+            );
+          })
         ) : (
-          <EmptyStateContainer>
-            <EmptyStateTitle>No public monitors</EmptyStateTitle>
-            <EmptyStateDescription>
+          <StatusEmptyState>
+            <StatusEmptyStateTitle>No public monitors</StatusEmptyStateTitle>
+            <StatusEmptyStateDescription>
               No public monitors have been added to this page.
-            </EmptyStateDescription>
-          </EmptyStateContainer>
+            </StatusEmptyStateDescription>
+          </StatusEmptyState>
         )}
       </StatusContent>
     </Status>
