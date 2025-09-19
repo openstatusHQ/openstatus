b00af4adc3f25a98f6f546302acf5789eb6f8275
Maximilian Kaske <maximilian@kaske.org>
2025-09-19 09:33:29 +0200

    chore: check

--- apps/status-page/src/app/(status-page)/[domain]/(public)/events/(list)/page.tsx
+++ apps/status-page/src/app/(status-page)/[domain]/(public)/events/(list)/page.tsx
@@ -21,7 +21,7 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { formatDate } from "@/lib/formatter";
-import { CircleCheck } from "lucide-react";
+import { Check } from "lucide-react";
 import Link from "next/link";
 
 // TODO: include ?filter=maintenance/reports
@@ -71,7 +71,11 @@ export default function Page() {
                     <StatusEventTitle className="inline-flex gap-1">
                       {report.title}
                       {isReportResolvedOnly ? (
-                        <CircleCheck className="mt-1 ml-1.5 size-4 shrink-0 text-success" />
+                        <div className="mt-1 ml-1.5">
+                          <div className="rounded-full border border-success/20 bg-success/10 p-0.5 text-success">
+                            <Check className="size-3 shrink-0" />
+                          </div>
+                        </div>
                       ) : null}
                     </StatusEventTitle>
                     {report.monitorsToStatusReports.length > 0 ? (
