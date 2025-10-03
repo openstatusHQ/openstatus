3e9aba347c4df75a8a466e5fd868dea40998448c
Maximilian Kaske <maximilian@kaske.org>
2025-10-03 22:30:49 +0200

    chore: seo

--- apps/web/src/app/(pages)/(content)/play/checker/_components/global-monitoring.tsx
+++ apps/web/src/app/(pages)/(content)/play/checker/_components/global-monitoring.tsx
@@ -26,7 +26,7 @@ const features: {
     icon: "globe",
     catchline: "Global Speed Test",
     description:
-      "Monitor latency performance in different regions to ensure quick load times for users across 35 regions around the world.",
+      "Monitor latency performance in different regions to ensure quick load times for users across 35 regions worldwide.",
   },
   {
     icon: "link",
