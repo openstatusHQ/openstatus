0dfa8fb1bc3b9d86457894c60b03b146d069f583
Maximilian Kaske <56969857+mxkaske@users.noreply.github.com>
2025-10-03 09:25:02 +0200

    chore: themes subdomain plausible (#1409)

--- apps/status-page/src/app/(public)/layout.tsx
+++ apps/status-page/src/app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import { Link } from "@/components/common/link";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
+import PlausibleProvider from "next-plausible";
 
 export const metadata: Metadata = {
   title: "Theme Explorer",
@@ -13,19 +14,21 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <main>{children}</main>
-      <footer className="flex items-center justify-center gap-4 p-4 text-center font-mono text-muted-foreground text-sm">
-        <p>
-          powered by <Link href="https://openstatus.dev">openstatus</Link>
-        </p>
-      </footer>
-      <Toaster richColors expand />
-    </ThemeProvider>
+    <PlausibleProvider domain="themes.openstatus.dev">
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <main>{children}</main>
+        <footer className="flex items-center justify-center gap-4 p-4 text-center font-mono text-muted-foreground text-sm">
+          <p>
+            powered by <Link href="https://openstatus.dev">openstatus</Link>
+          </p>
+        </footer>
+        <Toaster richColors expand />
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 }
