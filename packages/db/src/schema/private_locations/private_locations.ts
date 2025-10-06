8476b279c2463f086c8d562b73a77edf0e04f889
Thibault Le Ouay <thibaultleouay@gmail.com>
2025-10-06 21:59:14 +0200

    🚀

---
+++ packages/db/src/schema/private_locations/private_locations.ts
@@ -0,0 +1,28 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { workspace } from "../workspaces";
+import { sql } from "drizzle-orm/sql";
+import { monitor } from "../monitors/monitor";
+
+export const privateLocation = sqliteTable("private_location", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  key: text("key").notNull(),
+  lastSeenAt: integer("last_seen_at", { mode: "timestamp" }),
+  workspaceId: integer("workspace_id").references(() => workspace.id),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+})
+
+
+export const privateLocationToMonitors = sqliteTable("private_location_to_monitor", {
+  privateLocationId: integer("private_location_id").references(() => privateLocation.id),
+  monitorId: integer("monitor_id").references(() => monitor.id),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+  deletedAt: integer("deleted_at", { mode: "timestamp" })
+});
