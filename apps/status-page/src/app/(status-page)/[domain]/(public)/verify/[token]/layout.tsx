b1ce6fa73081ad49c21b00686535a44ff0f321b7
Maximilian Kaske <maximilian@kaske.org>
2025-10-09 16:13:19 +0200

    chore: improve verify page

---
+++ apps/status-page/src/app/(status-page)/[domain]/(public)/verify/[token]/layout.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  Status,
+  StatusContent,
+  StatusDescription,
+  StatusHeader,
+  StatusTitle,
+} from "@/components/status-page/status";
+import { useTRPC } from "@/lib/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
+export default function EventLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { domain } = useParams<{ domain: string }>();
+  const trpc = useTRPC();
+  const { data: page } = useQuery(
+    trpc.statusPage.get.queryOptions({ slug: domain }),
+  );
+
+  if (!page) return null;
+
+  return (
+    <Status>
+      <StatusHeader>
+        <StatusTitle>{page.title}</StatusTitle>
+        <StatusDescription>{page.description}</StatusDescription>
+      </StatusHeader>
+      <StatusContent>{children}</StatusContent>
+    </Status>
+  );
+}
