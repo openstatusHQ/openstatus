d4da3aa44ec372d4aa4f52c7a28777274aa2814b
Thibault Le Ouay <thibaultleouay@gmail.com>
2025-10-20 14:32:23 +0200

    rename

--- packages/db/src/sync-db.ts
+++ packages/db/src/sync-db.ts
@@ -4,8 +4,9 @@ import { createClient } from "@libsql/client";
 import { env } from "../env.mjs";
 import * as schema from "./schema";
 
+
 const client = createClient({
-  url: "file:/app/data/local.db",
+  url: "file:/app/data/replica.db",
   syncUrl: env.DATABASE_URL,
   authToken: env.DATABASE_AUTH_TOKEN,
   syncInterval: 60,
