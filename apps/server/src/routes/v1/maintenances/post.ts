7978a3ee79944dea12172993f84ced1b4f29a9ff
Maximilian Kaske <maximilian@kaske.org>
2025-11-16 16:02:32 +0100

    chore: api maintenance notification

--- apps/server/src/routes/v1/maintenances/post.ts
+++ apps/server/src/routes/v1/maintenances/post.ts
@@ -1,15 +1,19 @@
+import { env } from "@/env";
 import { OpenStatusApiError, openApiErrorResponses } from "@/libs/errors";
 import { trackMiddleware } from "@/libs/middlewares";
 import { createRoute } from "@hono/zod-openapi";
 import { Events } from "@openstatus/analytics";
-import { and, db, eq, inArray, isNull } from "@openstatus/db";
-import { monitor, page } from "@openstatus/db/src/schema";
+import { and, db, eq, inArray, isNotNull, isNull } from "@openstatus/db";
+import { monitor, page, pageSubscriber } from "@openstatus/db/src/schema";
 import {
   maintenance,
   maintenancesToMonitors,
 } from "@openstatus/db/src/schema/maintenances";
+import { EmailClient } from "@openstatus/emails";
 import type { maintenancesApi } from "./index";
 import { MaintenanceSchema } from "./schema";
+
+const emailClient = new EmailClient({ apiKey: env.RESEND_API_KEY });
 
 const postRoute = createRoute({
   method: "post",
@@ -43,6 +47,7 @@ export function registerPostMaintenance(api: typeof maintenancesApi) {
   return api.openapi(postRoute, async (c) => {
     const workspaceId = c.get("workspace").id;
     const input = c.req.valid("json");
+    const limits = c.get("workspace").limits;
 
     const { monitorIds, pageId } = input;
 
@@ -102,6 +107,45 @@ export function registerPostMaintenance(api: typeof maintenancesApi) {
 
       return newMaintenance;
     });
+
+    if (limits["status-subscribers"] && _maintenance.pageId) {
+      const subscribers = await db
+        .select()
+        .from(pageSubscriber)
+        .where(
+          and(
+            eq(pageSubscriber.pageId, _maintenance.pageId),
+            isNotNull(pageSubscriber.acceptedAt),
+          ),
+        )
+        .all();
+
+      const _page = await db.query.page.findFirst({
+        where: and(
+          eq(page.id, _maintenance.pageId),
+          eq(page.workspaceId, workspaceId),
+        ),
+        with: {
+          monitorsToPages: {
+            with: {
+              monitor: true,
+            },
+          },
+        },
+      });
+
+      if (_page && subscribers.length > 0) {
+        await emailClient.sendStatusReportUpdate({
+          to: subscribers.map((subscriber) => subscriber.email),
+          pageTitle: _page.title,
+          reportTitle: _maintenance.title,
+          status: "maintenance",
+          message: _maintenance.message,
+          date: _maintenance.from.toISOString(),
+          monitors: _page.monitorsToPages.map((i) => i.monitor.name),
+        });
+      }
+    }
 
     const data = MaintenanceSchema.parse({
       ..._maintenance,
