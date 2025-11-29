199c383ac3a885b98a970b5038fcd1ac3e3fb066
Ramtin Mesgari <26694963+iamramtin@users.noreply.github.com>
2025-11-29 14:47:03 +0700

    feat: Add Docker setup for Next.js apps - Add Dofigen and Dockerfile configs for status-page and dashboard Next.js applications - Update docker-compose to build all services
    Signed-off-by: Ramtin Mesgari <26694963+iamramtin@users.noreply.github.com>

--- apps/status-page/src/lib/trpc/shared.ts
+++ apps/status-page/src/lib/trpc/shared.ts
@@ -6,6 +6,7 @@ import superjson from "superjson";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
+  if (process.env.EXTERNAL_API_URL) return process.env.EXTERNAL_API_URL; // Docker
   const vc = process.env.VERCEL_URL;
   if (vc) return `https://${vc}`;
   // if (vc) return "https://app.openstatus.dev";
