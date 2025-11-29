56647b9a0a98da327abf502553fd1aaeab5e7766
Ramtin Mesgari <26694963+iamramtin@users.noreply.github.com>
2025-11-29 17:31:33 +0700

    feat: Add service communication configurations
    Signed-off-by: Ramtin Mesgari <26694963+iamramtin@users.noreply.github.com>

--- apps/dashboard/src/lib/trpc/shared.ts
+++ apps/dashboard/src/lib/trpc/shared.ts
@@ -7,10 +7,8 @@ import superjson from "superjson";
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
   if (process.env.EXTERNAL_API_URL) return process.env.EXTERNAL_API_URL; // Docker
-  const vc = process.env.VERCEL_URL;
-  // if (vc) return `https://${vc}`;
-  if (vc) return "https://app.openstatus.dev";
-  return "http://localhost:3000";
+  if (process.env.VERCEL_URL) return process.env.VERCEL_URL; // Vercel
+  return "http://localhost:3000"; // Local dev
 };
 
 const lambdas = ["stripeRouter", "emailRouter"];
--- apps/web/src/trpc/shared.ts
+++ apps/web/src/trpc/shared.ts
@@ -6,9 +6,9 @@ import superjson from "superjson";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
-  const vc = process.env.VERCEL_URL;
-  if (vc) return `https://${vc}`;
-  return "http://localhost:3000";
+  // if (process.env.EXTERNAL_API_URL) return process.env.EXTERNAL_API_URL; // Docker
+  if (process.env.VERCEL_URL) return process.env.VERCEL_URL; // Vercel
+  return "http://localhost:3000"; // Local dev
 };
 
 const lambdas = ["stripeRouter", "emailRouter"];
