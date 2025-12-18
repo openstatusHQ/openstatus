4e21c9f17c091e55fbcb7b33de1960e417d91a5f
Moulik Aggarwal <qwertymoulik@gmail.com>
2025-12-18 21:27:35 +0530

    Removed pointer css

--- apps/dashboard/src/components/data-table/private-locations/columns.tsx
+++ apps/dashboard/src/components/data-table/private-locations/columns.tsx
@@ -39,7 +39,7 @@ export const columns: ColumnDef<PrivateLocation>[] = [
       const value = row.getValue("monitors");
       if (Array.isArray(value) && value.length > 0 && "name" in value[0]) {
         return (
-          <div className="flex cursor-pointer flex-wrap gap-1">
+          <div className="flex flex-wrap gap-1">
             {value.map((m) => (
               <Link href={`/monitors/${m.id}`} key={m.id}>
                 <TableCellBadge value={m.name} />
