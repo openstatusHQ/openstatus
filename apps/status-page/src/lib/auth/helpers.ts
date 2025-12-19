43acc2b41364833378c7c33035e13663e7ba9d1d
Maximilian Kaske <maximilian@kaske.org>
2025-12-19 15:19:32 +0100

    fix: build

--- apps/status-page/src/lib/auth/helpers.ts
+++ apps/status-page/src/lib/auth/helpers.ts
@@ -1,8 +1,7 @@
 import type { AdapterUser } from "next-auth/adapters";
-import * as randomWordSlugs from "random-word-slugs";
 
 import { db, eq } from "@openstatus/db";
-import { user, usersToWorkspaces, workspace } from "@openstatus/db/src/schema";
+import { user } from "@openstatus/db/src/schema";
 
 export async function createUser(data: AdapterUser) {
   const { id, ...rest } = data;
@@ -15,38 +14,6 @@ export async function createUser(data: AdapterUser) {
       name: rest.name,
       firstName: rest.firstName,
       lastName: rest.lastName,
-    })
-    .returning()
-    .get();
-
-  let slug: string | undefined = undefined;
-
-  while (!slug) {
-    slug = randomWordSlugs.generateSlug(2);
-    const slugAlreadyExists = await db
-      .select()
-      .from(workspace)
-      .where(eq(workspace.slug, slug))
-      .get();
-
-    if (slugAlreadyExists) {
-      console.log(`slug already exists: '${slug}'`);
-      slug = undefined;
-    }
-  }
-
-  const newWorkspace = await db
-    .insert(workspace)
-    .values({ slug, name: "" })
-    .returning({ id: workspace.id })
-    .get();
-
-  await db
-    .insert(usersToWorkspaces)
-    .values({
-      userId: newUser.id,
-      workspaceId: newWorkspace.id,
-      role: "owner",
     })
     .returning()
     .get();
