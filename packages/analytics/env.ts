ba551c065943718a8c723e8408043fdee815a961
Thibault Le Ouay <thibaultleouay@gmail.Com>
2026-01-02 22:30:51 +0100

    Revert "fix zod migration (#1685)"
    This reverts commit 9c2ed19e8bea0623aabd427521c22e070b43f957.

--- packages/analytics/env.ts
+++ packages/analytics/env.ts
@@ -1,5 +1,5 @@
 import { createEnv } from "@t3-oss/env-core";
-import { z } from "zod/v4";
+import { z } from "zod";
 
 export const env = createEnv({
   server: {
@@ -9,10 +9,9 @@ export const env = createEnv({
     NEXT_PUBLIC_OPENPANEL_CLIENT_ID: z.string(),
   },
   clientPrefix: "NEXT_PUBLIC_",
-
-  runtimeEnv: process.env,
-
-  skipValidation: true,
-
-  // runtimeEnv: process.env,
+  runtimeEnv: {
+    OPENPANEL_CLIENT_SECRET: process.env.OPENPANEL_CLIENT_SECRET,
+    NEXT_PUBLIC_OPENPANEL_CLIENT_ID:
+      process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID,
+  },
 });
