8097e7ad01a66e5b27b78542dca7f9edbda8484b
Thibault Le Ouay Ducasse <thibaultleouay@gmail.com>
2026-01-07 12:06:34 +0100

    fix build

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,5 @@
 export * as schema from "./schema";
 export * from "drizzle-orm";
 export * from "./db";
-export * from "./utils";
 // doing this because the external module not working see : https://github.com/vercel/next.js/issues/43433
 // export * from "./sync-db";
