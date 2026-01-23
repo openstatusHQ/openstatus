e58f852c1a29d4860200f203cdae4372ee16c2a0
Maximilian Kaske <maximilian@kaske.org>
2026-01-23 12:10:32 +0100

    fix: undefined res

--- apps/checker/handlers/checker.go
+++ apps/checker/handlers/checker.go
@@ -308,7 +308,6 @@ func (h Handler) HTTPCheckerHandler(c *gin.Context) {
 				Message:       err.Error(),
 				Region:        h.Region,
 				CronTimestamp: req.CronTimestamp,
-				Latency:       res.Latency,
 			})
 		}
 	}
