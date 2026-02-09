651b8c882f7b5ac4d5a9c41c037a4a414691c60a
Maximilian Kaske <maximilian@kaske.org>
2026-02-09 19:52:06 +0100

    wip: blog component registry

--- apps/web/src/app/api/search/route.ts
+++ apps/web/src/app/api/search/route.ts
@@ -71,6 +71,7 @@ function search(params: SearchParams) {
       ...getPages("compare"),
       ...getPages("product"),
       ...getPages("guides"),
+      ...getPages("unrelated"),
       home,
     ];
   } else {
