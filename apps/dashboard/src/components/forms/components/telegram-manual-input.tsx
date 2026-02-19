9355cc4ad7c43345098b7c2578219245b5a28021
Moulik Aggarwal <qwertymoulik@gmail.com>
2026-02-19 10:10:04 +0530

    Update code
    Signed-off-by: Moulik Aggarwal <qwertymoulik@gmail.com>

---
+++ apps/dashboard/src/components/forms/components/telegram-manual-input.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@openstatus/ui/components/ui/form";
+import { Input } from "@openstatus/ui/components/ui/input";
+import { Link } from "@/components/common/link";
+import { UseFormReturn } from "react-hook-form";
+
+interface TelegramManualInputProps {
+  form: UseFormReturn<any>;
+}
+
+export function TelegramManualInput({ form }: TelegramManualInputProps) {
+  return (
+    <FormField
+      control={form.control}
+      name="data.chatId"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Telegram Chat ID</FormLabel>
+          <FormControl>
+            <Input placeholder="1234567890" {...field} />
+          </FormControl>
+          <FormMessage />
+          <FormDescription>
+            Enter the Telegram chat ID to send notifications to.{" "}
+            <Link
+              href="https://docs.openstatus.dev/reference/notification/#telegram"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Learn more
+            </Link>
+          </FormDescription>
+        </FormItem>
+      )}
+    />
+  );
+}
---
+++ packages/ui/src/components/ui/qr-code.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import QRCodeStyling, {
+  type Options as QRCodeOptions,
+  type FileExtension,
+} from "qr-code-styling";
+
+import { cn } from "../../lib/utils";
+
+export interface QRCodeProps {
+  data: string;
+  image?: string;
+  size?: number;
+  className?: string;
+  options?: Partial<QRCodeOptions>;
+}
+
+export const QRCode = ({
+  data,
+  image,
+  size = 200,
+  className,
+  options,
+}: QRCodeProps) => {
+  const [qrCode] = useState<QRCodeStyling>(
+    new QRCodeStyling({
+      width: size,
+      height: size,
+      type: "svg",
+      data,
+      image,
+      dotsOptions: {
+        color: "#000000",
+        type: "rounded",
+      },
+      backgroundOptions: {
+        color: "#ffffff",
+      },
+      imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 20,
+      },
+      ...options,
+    }),
+  );
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      qrCode.append(ref.current);
+    }
+  }, [qrCode, ref]);
+
+  useEffect(() => {
+    if (!qrCode) return;
+    qrCode.update({
+      data,
+      image,
+      width: size,
+      height: size,
+      ...options,
+    });
+  }, [qrCode, data, image, size, options]);
+
+  return (
+    <div
+      ref={ref}
+      className={cn("flex items-center justify-center", className)}
+    />
+  );
+};
