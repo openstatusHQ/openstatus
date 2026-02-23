c0a1a0511d82dbf18b64489b30e63cc653b60728
Thibault Le Ouay Ducasse <thibaultleouay@gmail.com>
2026-02-23 20:55:21 +0100

    fix router

--- apps/dashboard/src/lib/trpc/shared.ts
+++ apps/dashboard/src/lib/trpc/shared.ts
@@ -11,7 +11,7 @@ const getBaseUrl = () => {
   return "http://localhost:3000"; // Local dev and Docker (internal calls)
 };
 
-const lambdas = ["stripeRouter", "emailRouter", "apiKeyRouter"];
+const lambdas = ["stripeRouter", "emailRouter", "apiKeyRouter", "integrationRouter"];
 
 export const endingLink = (opts?: {
   fetch?: typeof fetch;
