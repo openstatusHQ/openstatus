24b66a8eb2174da46230ad5cb9f5305ef2b9e849
Maximilian Kaske <maximilian@kaske.org>
2026-04-02 14:37:02 +0200

    fix: early return

--- packages/api/src/router/feedback.ts
+++ packages/api/src/router/feedback.ts
@@ -3,7 +3,6 @@ import {
   feedbackSource,
   feedbackType,
 } from "@openstatus/db/src/schema";
-import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { env } from "../env";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
@@ -41,10 +40,8 @@ export const feedbackRouter = createTRPCRouter({
 
       // Send to Slack
       if (!env.SLACK_FEEDBACK_WEBHOOK_URL) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Slack feedback webhook not configured.",
-        });
+        console.error("Slack feedback webhook not configured.");
+        return { success: true } as const;
       }
 
       if (process.env.NODE_ENV === "development") {
