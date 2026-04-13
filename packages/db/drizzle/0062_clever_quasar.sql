ad4b19136435a216596965fa9c4859a3a89af40a
Maximilian Kaske <maximilian@kaske.org>
2026-04-13 07:43:59 +0200

    fix: migration

--- packages/db/drizzle/0062_clever_quasar.sql
+++ packages/db/drizzle/0062_clever_quasar.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `page` ADD `allow_index` integer DEFAULT true NOT NULL;--> statement-breakpoint
-UPDATE `page` SET `allow_index` = 0 WHERE `access_type` != 'public' OR `access_type` IS NULL;
+ALTER TABLE `page` ADD `allow_index` integer DEFAULT true NOT NULL;
