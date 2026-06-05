03c6d4eeb8ac903a43d6e65f4cbca1ce5a681193
Maximilian Kaske <maximilian@kaske.org>
2026-06-05 11:28:58 +0200

    chore: fab

--- apps/web/src/content/docs-sidebar.tsx
+++ apps/web/src/content/docs-sidebar.tsx
@@ -213,9 +213,9 @@ export function DocsMobileNav({ className }: { className?: string }) {
           className="fixed right-4 bottom-4 z-40"
           asChild
         >
-          <Button className="rounded-none p-4 font-mono">
+          <Button className="h-auto rounded-none py-3 font-mono has-[>svg]:px-4">
             <Menu className="size-4" />
-            Docs menu
+            Menu
           </Button>
         </DrawerTrigger>
         <DrawerContent className="rounded-none! font-sans">
