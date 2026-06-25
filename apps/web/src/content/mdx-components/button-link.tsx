6148974f0194c4a044eb5c742b5a1a5ec10ff537
Maximilian Kaske <maximilian@kaske.org>
2026-06-25 14:38:57 +0200

    chore: home primary cta button

--- apps/web/src/content/mdx-components/button-link.tsx
+++ apps/web/src/content/mdx-components/button-link.tsx
@@ -10,7 +10,7 @@ export function ButtonLink(
     <Button
       variant="outline"
       size="lg"
-      className="h-auto rounded-none px-4 py-4 text-base no-underline!"
+      className="data-[variant=default]:text-background! h-auto rounded-none px-4 py-4 text-base no-underline!"
       asChild
       {...props}
     >
