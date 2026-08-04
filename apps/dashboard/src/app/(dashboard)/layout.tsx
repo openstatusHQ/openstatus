5dca7358b48eb342df45696000d24bc8a95e168d
Maximilian Kaske <maximilian@kaske.org>
2026-08-04 21:42:07 +0200

    fix: artificial delay

--- apps/dashboard/src/app/(dashboard)/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/layout.tsx
@@ -54,7 +54,5 @@ async function HydrateSidebar({ children }: { children: React.ReactNode }) {
     queryClient.prefetchQuery(trpc.user.get.queryOptions()),
   ]);
 
-  await new Promise((resolve) => setTimeout(resolve, 1000)); // Ensure hydration happens after prefetching
-
   return <HydrateClient>{children}</HydrateClient>;
 }
