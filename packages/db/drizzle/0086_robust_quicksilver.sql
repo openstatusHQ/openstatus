1872556f02937d0e948ef385efa6df0ae2eeb3ca
Maximilian Kaske <maximilian@kaske.org>
2026-09-06 21:03:46 +0200

    fix: migration files

--- packages/db/drizzle/0086_robust_quicksilver.sql
+++ packages/db/drizzle/0086_robust_quicksilver.sql
@@ -22,6 +22,8 @@ CREATE TABLE `oauth_session` (
 	FOREIGN KEY (`client_id`) REFERENCES `oauth_client`(`client_id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
+CREATE INDEX `oauth_session_client_id_idx` ON `oauth_session` (`client_id`);--> statement-breakpoint
+CREATE INDEX `oauth_session_expires_at_idx` ON `oauth_session` (`expires_at`);--> statement-breakpoint
 CREATE TABLE `oauth_authorization_code` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`hash` text NOT NULL,
@@ -42,6 +44,7 @@ CREATE TABLE `oauth_authorization_code` (
 --> statement-breakpoint
 CREATE UNIQUE INDEX `oauth_authorization_code_hash_unique` ON `oauth_authorization_code` (`hash`);--> statement-breakpoint
 CREATE INDEX `oauth_authorization_code_expires_at_idx` ON `oauth_authorization_code` (`expires_at`);--> statement-breakpoint
+CREATE INDEX `oauth_authorization_code_client_id_idx` ON `oauth_authorization_code` (`client_id`);--> statement-breakpoint
 CREATE TABLE `oauth_grant` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`client_id` text NOT NULL,
@@ -65,5 +68,7 @@ CREATE TABLE `oauth_grant` (
 CREATE UNIQUE INDEX `oauth_grant_access_token_hash_unique` ON `oauth_grant` (`access_token_hash`);--> statement-breakpoint
 CREATE UNIQUE INDEX `oauth_grant_refresh_token_hash_unique` ON `oauth_grant` (`refresh_token_hash`);--> statement-breakpoint
 CREATE INDEX `oauth_grant_workspace_id_idx` ON `oauth_grant` (`workspace_id`);--> statement-breakpoint
-CREATE INDEX `oauth_grant_client_id_idx` ON `oauth_grant` (`client_id`);--> statement-breakpoint
-CREATE INDEX `oauth_grant_previous_refresh_token_hash_idx` ON `oauth_grant` (`previous_refresh_token_hash`);
+CREATE INDEX `oauth_grant_client_id_user_id_idx` ON `oauth_grant` (`client_id`,`user_id`);--> statement-breakpoint
+CREATE INDEX `oauth_grant_user_id_idx` ON `oauth_grant` (`user_id`);--> statement-breakpoint
+CREATE INDEX `oauth_grant_previous_refresh_token_hash_idx` ON `oauth_grant` (`previous_refresh_token_hash`);--> statement-breakpoint
+CREATE INDEX `oauth_grant_refresh_token_expires_at_idx` ON `oauth_grant` (`refresh_token_expires_at`);
